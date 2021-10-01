ea4cfa23d948c36118fa2dcfc3d20e8b17db5a45
Jayant Goel <jgoel92@gmail.com>
2021-10-01 17:34:55 +0530

    Time: 44 ms (36.12%), Space: 21 MB (30.82%) - LeetHub

---
+++ contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        map<int,int> mp;
+        int size = nums.size();
+        
+        for(int i = 0;i<size;i++){
+            if(mp[nums[i]] !=0 && mp[nums[i]] != i + 1){
+               return true; 
+            }
+            mp[nums[i]] = i + 1;
+        }
+        return false;
+    }
+};
