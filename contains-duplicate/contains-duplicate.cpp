3b34fd8176a14813621013b6e70ed893c0f99d7c
Jayant Goel <jgoel92@gmail.com>
2021-10-06 02:02:58 +0530

    Time: 32 ms (67.84%), Space: 20.1 MB (61.05%) - LeetHub

--- contains-duplicate/contains-duplicate.cpp
+++ contains-duplicate/contains-duplicate.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        map<int,int> mp;
+        unordered_map<int,int> mp;
         int size = nums.size();
         
         for(int i = 0;i<size;i++){
