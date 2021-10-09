89e0baa022232e9386aa3b2ee7b9c66b5c9cf0cf
Jayant Goel <jgoel92@gmail.com>
2021-10-09 12:25:45 +0530

    Time: 24 ms (54.76%), Space: 20.6 MB (17.76%) - LeetHub

--- valid-sudoku/valid-sudoku.cpp
+++ valid-sudoku/valid-sudoku.cpp
@@ -1,52 +1,40 @@
 class Solution {
 public:
-    bool isSafe(vector<vector<char>>& board,int x,int y,int row,int col){
-        unordered_map<char,int> mp;
-        
-        for(int i = 0;i<9;i++){
-            if(board[i][y] != '.'){
-                mp[board[i][y]]++;
-                if(mp[board[i][y]] > 1){
-                    return false;
-                }
-            }
-        }
-        mp.clear();
-        
-        for(int i = 0;i<9;i++){
-            if(board[x][i] != '.'){
-                mp[board[x][i]]++;
-                if(mp[board[x][i]] > 1){
-                    return false;
-                }
-            }
-        }
-        mp.clear();
-        int _x = x - x%3;
-        int _y = y - y%3;
-        
-        for(int i=_x;i<_x + 3;i++){
-            for(int j=_y;j<_y + 3;j++){
-                if(board[i][j] != '.'){
-                    mp[board[i][j]]++;
-                    if(mp[board[i][j]] > 1){
-                        return false;
-                    }
-                }
-            }
-        }
-        return true;
-        
-    }
-    
     bool isValidSudoku(vector<vector<char>>& board) {
-        int r = board.size();
-        int c = board[0].size();
-        
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                if(!isSafe(board,i,j,r,c)){
+        for(int i=0;i<9;i++){
+            unordered_map<char,int> mp;
+            for(int j=0;j<9;j++){
+                if(board[i][j]!='.'){
+                    mp[board[i][j]]++;
+                }
+                if(mp[board[i][j]] > 1){
                     return false;
+                }
+            }
+        }
+        for(int i=0;i<9;i++){
+            unordered_map<char,int> mp;
+            for(int j=0;j<9;j++){
+                if(board[j][i]!='.'){
+                    mp[board[j][i]]++;
+                }
+                if(mp[board[j][i]] > 1){
+                    return false;
+                }
+            }
+        }
+        for(int i=0;i<9;i+=3){
+            for(int j=0;j<9;j+=3){
+                unordered_map<char,int> mp;
+                for(int k=i;k<i+3;k++){
+                    for(int l=j;l<j+3;l++){
+                        if(board[k][l]!='.'){
+                            mp[board[k][l]]++;
+                        }
+                        if(mp[board[k][l]] > 1){
+                            return false;
+                        }
+                    }
                 }
             }
         }
