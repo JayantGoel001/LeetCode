718f217f7685352a307d0791241d832367b2b0ae
Jayant Goel <jgoel92@gmail.com>
2021-10-10 05:13:55 +0530

    Time: 12 ms (89.53%), Space: 8 MB (99.82%) - LeetHub

--- ransom-note/ransom-note.cpp
+++ ransom-note/ransom-note.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     bool canConstruct(string &ransomNote, string &magazine) {
-        unordered_map<char,int> mp;
+        
+        int mp[26];
+        for(int i=0;i<26;i++){
+            mp[i] = 0;
+        }
         
         for(auto it : magazine){
-            mp[it]++;
+            mp[it-'a']++;
         }
         for(auto it : ransomNote){
-            mp[it]--;
-            if(mp[it] < 0){
+            mp[it-'a']--;
+            if(mp[it-'a'] < 0){
                 return false;
             }
         }
