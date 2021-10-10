110794a1ae9a0563beafa3014d6d1122fd11f783
Jayant Goel <jgoel92@gmail.com>
2021-10-10 07:08:13 +0530

    Time: 5 ms (83.84%), Space: 6.9 MB (99.75%) - LeetHub

--- permutation-in-string/permutation-in-string.cpp
+++ permutation-in-string/permutation-in-string.cpp
@@ -1,26 +1,38 @@
 class Solution {
 public:
-    bool checkInclusion(string &s1, string &s2) {
-        unordered_map<char,int> mp,temp;
-        
-        for(auto it : s1){
-            mp[it]++;
+    bool cmp(int *mp,int *temp){
+        for(int i=0;i<26;i++){
+            if(mp[i] != temp[i]){
+                return false;
+            }
         }
-        int count = 0;
+        return true;
+    }
+    bool checkInclusion(string &s1, string &s2) {
         
         int k = s1.size();
         int size = s2.size();
         
+        if(k > size){
+            return false;
+        }
+        int mp[26],temp[26];
+        for(int i=0;i<26;i++){
+            mp[i] = 0;
+            temp[i] = 0;
+        }
+        
+        for(auto it : s1){
+            mp[it - 'a']++;
+        }
+        int count = 0;
+        
         for(int i=0;i<size;i++){
-            temp[s2[i]]++;
+            temp[s2[i] - 'a']++;
             if(i >= k){
-                temp[s2[i - k]]--;
-                
-                if(temp[s2[i - k]] == 0){
-                    temp.erase(s2[i - k]);
-                }
+                temp[s2[i - k] - 'a']--;
             }
-            if(mp == temp){
+            if(cmp(mp,temp)){
                 return true;
             }
         }
