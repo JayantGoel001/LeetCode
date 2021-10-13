fa2cc3c0a063ce87f77c85aac8b4249fb2cfdb3c
Jayant Goel <jgoel92@gmail.com>
2021-10-13 20:17:58 +0530

    Time: 24 ms (32.11%), Space: 14.2 MB (30.62%) - LeetHub

--- word-break/word-break.cpp
+++ word-break/word-break.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
-    bool wordBreak(string s, vector<string>& wordDict) {
-        
+    bool wordBreak(string &s, vector<string>& wordDict) {
+        int size = s.size();
         sort(wordDict.begin(),wordDict.end());
+        vector<bool> dp(size+1,false);
         
-        int ssize = s.size();
-        bool dp[ssize+1];
-        
-        for(int i=0;i<=ssize;i++){
-            dp[i] = false;
-        }
-        dp[0] = true;
-        
-        int size = wordDict.size();
-        for(int i=1;i<=ssize;i++){
-            for(int j=0;j<i;j++){
+        dp[0]=true;
+        int n = wordDict.size();
+        for(int i =1;i<=size;i++){
+            for(int j =0;j<i;j++){
                 string str = s.substr(j,i-j);
-                int pos = lower_bound(wordDict.begin(),wordDict.end(),str) - wordDict.begin();
-                if(dp[j] && pos!=size && wordDict[pos] == str){
-                    dp[i] = true;
+                auto pos = lower_bound(wordDict.begin(),wordDict.end(),str);
+                if(dp[j] && pos!=wordDict.end() && wordDict[pos - wordDict.begin()] == str){
+                    dp[i]=true;
+                    break;
                 }
             }
         }
-        return dp[ssize];
+        return dp[size];
     }
 };
