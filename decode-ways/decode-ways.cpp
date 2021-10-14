56e8194c6a4bf1b9a7983763f19777f0d6b20087
Jayant Goel <jgoel92@gmail.com>
2021-10-14 22:03:51 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (99.95%) - LeetHub

--- decode-ways/decode-ways.cpp
+++ decode-ways/decode-ways.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    int numDecodings(string s) {
-        if(s[0] == '0'){
+    int numDecodings(string &str){
+        if(str[0] == '0'){
             return 0;
         }
         int prev = 1;
-        int prevP = 1;
+        int prevPrev = 1;
         
-        int size = s.size();
+        int size = str.size();
         for(int i=1;i<size;i++){
             int temp = 0;
             
-            if(s[i]>'0' && s[i] <= '9'){
+            if(str[i] > '0' && str[i] <= '9'){
                 temp += prev;
             }
-            int number = (s[i-1]-'0') * 10 + (s[i] - '0');
-        
-            if(number >=10 && number <= 26){
-                temp += prevP;
+            int num = (str[i-1] - '0') * 10 + (str[i] - '0');
+            if(num >= 10 && num <=26){
+                temp += prevPrev;
             }
-            prevP = prev;
+            prevPrev = prev;
             prev = temp;
         }
         return prev;
