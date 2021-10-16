1d8a00cd27fbcd05526b397edcf9412076f591ef
Jayant Goel <jgoel92@gmail.com>
2021-10-16 12:34:07 +0530

    Time: 3 ms (26.83%), Space: 6.7 MB (18.28%) - LeetHub

--- pascals-triangle-ii/pascals-triangle-ii.cpp
+++ pascals-triangle-ii/pascals-triangle-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> getRow(int rowIndex) {
-        vector<vector<int>> v;
+        vector<int> v;
         
         v.push_back({1});
         for(int i = 1;i<=rowIndex;i++){
@@ -9,13 +9,13 @@ public:
             v2.push_back(1);
             
             for(int j = 1;j<i;j++){
-                v2.push_back(v[i-1][j-1] + v[i-1][j]);
+                v2.push_back(v[j-1] + v[j]);
             }
             
             v2.push_back(1);
             
-            v.push_back(v2);
+            v = v2;
         }
-        return v[rowIndex];
+        return v;
     }
 };
