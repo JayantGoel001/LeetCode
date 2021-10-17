1a21c88f818a1e2bbdcb861f92e06ddc403b99c1
Jayant Goel <jgoel92@gmail.com>
2021-10-17 18:04:20 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (7.94%) - LeetHub

---
+++ to-lower-case/to-lower-case.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+        string str = "";
+        for(auto it : s){
+            if(it>='A' && it<='Z'){
+                str += (it + 32);
+            }else{
+                str+=it;
+            }
+        }
+        return str;
+    }
+};
