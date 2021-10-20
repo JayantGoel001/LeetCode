e3b21b15a8b2198eb029e4ae17209d2e234017f2
Jayant Goel <jgoel92@gmail.com>
2021-10-20 13:52:18 +0530

    Time: 4 ms (12.75%), Space: 6 MB (38.47%) - LeetHub

---
+++ xor-operation-in-an-array/xor-operation-in-an-array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int xorOperation(int n, int start) {
+        int res = 0;
+        
+        for(int i =0;i<n;i++){
+            res = res ^ (start + 2*i);
+        }
+        return res;
+    }
+};
