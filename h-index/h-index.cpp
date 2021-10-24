f3a958c0fcfbcb49c65515e6d0f14b9cd7bc0cca
Jayant Goel <jgoel92@gmail.com>
2021-10-24 14:24:25 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (79.86%) - LeetHub

--- h-index/h-index.cpp
+++ h-index/h-index.cpp
@@ -1,14 +1,33 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-        sort(citations.begin(),citations.end());
-        
         int size = citations.size();
-        for(int i=0;i<size;i++){
-            if(citations[i] >= size - i){
-                return size-i;
+        int ar[size+1];
+        for(int i =0;i<=size;i++){
+            ar[i]=0;
+        }
+        for(auto it : citations){
+            ar[min(it,size)]++;
+        }
+        int i = size-1;
+        while(i>=0){
+            ar[i]+=ar[i+1];
+            if(ar[i+1]>=i+1){
+                return i+1;
             }
+            i--;
         }
         return 0;
     }
+//     int hIndex(vector<int>& citations) {
+//         sort(citations.begin(),citations.end());
+        
+//         int size = citations.size();
+//         for(int i=0;i<size;i++){
+//             if(citations[i] >= size - i){
+//                 return size-i;
+//             }
+//         }
+//         return 0;
+//     }
 };
