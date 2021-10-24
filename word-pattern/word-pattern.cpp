0db19dbd81ca9846fe2f44b33cab678d27d9a85f
Jayant Goel <jgoel92@gmail.com>
2021-10-24 16:05:58 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (54.29%) - LeetHub

--- word-pattern/word-pattern.cpp
+++ word-pattern/word-pattern.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     bool wordPattern(string pattern, string s) {
-        int size = pattern.size();
-        
-        unordered_map<char,string> mp;
         unordered_map<string,char> rmp;
-        vector<string> v;
+        unordered_map<char,string> mp;
         
+        int size = pattern.size();
+        int k = 0;
+        int ss = s.size();
+        vector<string> v;
         string str = "";
-        for(auto it : s){
-            if(it == ' '){
+        while(k<ss){
+            if(s[k] != ' '){
+                str += s[k];
+            }else{
                 v.push_back(str);
                 str = "";
-            }else{
-                str += it;
             }
+            k++;
         }
         v.push_back(str);
-        
-        if(v.size() != pattern.size()){
+        if(v.size() != size){
             return false;
         }
-        
         for(int i=0;i<size;i++){
             if((mp[pattern[i]]!="" && mp[pattern[i]] != v[i]) || (rmp[v[i]] && rmp[v[i]] != pattern[i])){
                 return false;
