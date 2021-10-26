0e2d1de869bd69cf044c8da745f352d4532df379
Jayant Goel <jgoel92@gmail.com>
2021-10-26 15:13:56 +0530

    Time: 4 ms (54.97%), Space: 7.3 MB (47.05%) - LeetHub

--- subsets/subsets.cpp
+++ subsets/subsets.cpp
@@ -2,23 +2,17 @@ class Solution {
 public:
     vector<vector<int>> v;
     void generateSubSet(vector<int>& nums,int x,int n,vector<int> &sub){
-        if(x == n){
-            v.push_back(sub);
-        }else{
-            for(int i=x;i<n;i++){
-                sub.push_back(nums[i]);
-                generateSubSet(nums,i + 1,n,sub);
-                sub.pop_back();
-            }
+        v.push_back(sub);
+        for(int i=x;i<n;i++){
+            sub.push_back(nums[i]);
+            generateSubSet(nums,i + 1,n,sub);
+            sub.pop_back();
         }
     }
     vector<vector<int>> subsets(vector<int>& nums) {
         int size = nums.size();
-        
-        for(int i=0;i<=size;i++){
-            vector<int> sub;
-            generateSubSet(nums,0,i,sub);
-        }
+        vector<int> sub;
+        generateSubSet(nums,0,size,sub);
         return v;
     }
 };
