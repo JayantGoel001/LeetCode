338bb9734252b876188c21eea273da771b39db64
Jayant Goel <jgoel92@gmail.com>
2021-10-27 13:34:00 +0530

    Time: 44 ms (24.46%), Space: 9.1 MB (59.76%) - LeetHub

--- permutations-ii/permutations-ii.cpp
+++ permutations-ii/permutations-ii.cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
     set<vector<int>> v;
-    int n;
-
-    void permutation(vector<int>& nums,int x){
+    void generatePermutations(vector<int>& nums,int x,int n){
         if(x == n){
             v.insert(nums);
-
         }else{
-            for(int i = x;i<n;i++){
-                swap(nums[i],nums[x]);
-
-                permutation(nums,x+1);
-
-                swap(nums[i],nums[x]);
+            for(int i=x;i<n;i++){
+                swap(nums[x],nums[i]);
+                generatePermutations(nums,x+1,n);
+                swap(nums[x],nums[i]);
             }
         }
     }
-
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        n = nums.size();
-
-        permutation(nums,0);
-        
+        int n = nums.size();
+        vector<int> per;
+        generatePermutations(nums,0,n);
         return vector<vector<int>>(v.begin(),v.end());
     }
 };
