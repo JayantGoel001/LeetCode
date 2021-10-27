224f6a5f86f14ba78c6bed1b79a1ec476c3030ce
Jayant Goel <jgoel92@gmail.com>
2021-10-27 13:38:27 +0530

    Time: 8 ms (72.27%), Space: 10.8 MB (89.70%) - LeetHub

--- combination-sum/combination-sum.cpp
+++ combination-sum/combination-sum.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> v;
-    
-    void generateSum(vector<int> &cand,int target,vector<int> &_v,int x=0) {
+    void generateComb(vector<int> &comb,int target,vector<int> &_v,int n,int x = 0){
         if(target == 0){
             v.push_back(_v);
         }else if(target > 0){
-            for(int i=x;i<cand.size();i++){
-                _v.push_back(cand[i]);
-                generateSum(cand,target - cand[i],_v,i);
+            for(int i=x;i<n;i++){
+                _v.push_back(comb[i]);
+                generateComb(comb,target - comb[i],_v,n,i);
                 _v.pop_back();
             }
         }
     }
-    
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+    vector<vector<int>> combinationSum(vector<int>& cand, int target) {
         vector<int> _v;
-        generateSum(candidates,target,_v);
+        
+        generateComb(cand,target,_v,cand.size());
         return v;
     }
 };
