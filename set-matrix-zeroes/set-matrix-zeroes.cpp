425afa190c3bae171c3ae48ae29fe4c6fee78c4b
Jayant Goel <jgoel92@gmail.com>
2021-10-29 02:41:10 +0530

    Time: 12 ms (87.96%), Space: 13.3 MB (54.54%) - LeetHub

--- set-matrix-zeroes/set-matrix-zeroes.cpp
+++ set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -3,18 +3,19 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         bool isRow = false;
         bool isCol = false;
-        
         int n = matrix.size();
         int m = matrix[0].size();
         
         for(int i=0;i<n;i++){
-            if(matrix[i][0]==0){
+            if(matrix[i][0] == 0){
                 isRow = true;
+                break;
             }
         }
-        for(int i=0;i<m;i++){
-            if(matrix[0][i]==0){
+        for(int j=0;j<m;j++){
+            if(matrix[0][j] == 0){
                 isCol = true;
+                break;
             }
         }
         for(int i=1;i<n;i++){
@@ -27,7 +28,7 @@ public:
         }
         for(int i=1;i<n;i++){
             for(int j=1;j<m;j++){
-                if(matrix[i][0] == 0 || matrix[0][j]==0){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
                     matrix[i][j] = 0;
                 }
             }
@@ -38,8 +39,8 @@ public:
             }
         }
         if(isCol){
-            for(int i=0;i<m;i++){
-                matrix[0][i] = 0;
+            for(int j=0;j<m;j++){
+                matrix[0][j] = 0;
             }
         }
     }
