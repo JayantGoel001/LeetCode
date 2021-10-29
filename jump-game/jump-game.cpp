be1199b18eaa65c721f7622abc3cebb7653f81c8
Jayant Goel <jgoel92@gmail.com>
2021-10-29 11:29:20 +0530

    Time: 44 ms (98.57%), Space: 48.2 MB (94.89%) - LeetHub

--- jump-game/jump-game.cpp
+++ jump-game/jump-game.cpp
@@ -2,15 +2,13 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int maxi = 0;
+        int n = nums.size();
         
-        int size = nums.size();
-        for(int i = 0;i<size;i++){
+        for(int i=0;i<n;i++){
             if(maxi < i){
                 return false;
             }
-            
-            maxi = max(maxi,i + nums[i]);
-        
+            maxi = max(maxi,nums[i] + i);
         }
         return true;
     }
