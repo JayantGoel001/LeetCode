7056a6e252430d38cde6233a48783d148d4e32ff
Jayant Goel <jgoel92@gmail.com>
2021-10-30 12:30:53 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (39.26%) - LeetHub

--- unique-paths/unique-paths.cpp
+++ unique-paths/unique-paths.cpp
@@ -1,28 +1,17 @@
 class Solution {
 public:
-    int nCr(int n,int r,int **dp){
-        if(n==1 || 1==r){
+    int getPaths(int m,int n,vector<vector<int>> &dp){
+        if(m==1 || n == 1){
             return 1;
         }
-        if(dp[n][r] != -1){
-            return dp[n][r];
+        if(dp[m][n]==-1){
+            dp[m][n] = getPaths(m-1,n,dp) + getPaths(m,n-1,dp);
         }
-        dp[n][r] = nCr(n,r-1,dp) + nCr(n-1,r,dp);
-        return dp[n][r];
+        return dp[m][n];
     }
-    
-    int uniquePaths(int x, int y) {
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
         
-        int **dp = new int*[x+1];
-        for(int i=0;i<=x;i++){
-            dp[i] = new int[y+1];
-        }
-        
-        for(int i=0;i<=x;i++){
-            for(int j=0;j<=y;j++){
-                dp[i][j] = -1;
-            }
-        }
-        return nCr(x,y,dp);
+        return getPaths(m,n,dp);
     }
 };
