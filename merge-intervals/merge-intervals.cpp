2376543b4230d568f825078caf1e0e2e32cfca14
Jayant Goel <jgoel92@gmail.com>
2021-11-18 00:46:17 +0530

    Time: 16 ms (89.59%), Space: 14.8 MB (31.88%) - LeetHub

--- merge-intervals/merge-intervals.cpp
+++ merge-intervals/merge-intervals.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        vector<vector<int>> merged;
+        vector<vector<int>> v;
         sort(intervals.begin(),intervals.end());
         for(auto it : intervals){
-            if(merged.empty() || merged.back()[1] < it[0]){
-                merged.push_back(it);
+            if(v.empty() || v.back()[1] < it[0]){
+                v.push_back(it);
             }else{
-                merged.back()[1] = max(merged.back()[1],it[1]);
+                v.back()[1] = max(v.back()[1],it[1]);
             }
         }
-        return merged;
+        return v;
     }
 };
