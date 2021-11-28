a88bd3125e384de78d9586e53a32ac597770117b
Jayant Goel <jgoel92@gmail.com>
2021-11-28 13:57:19 +0530

    Time: 12 ms (83.33%), Space: 12.1 MB (49.24%) - LeetHub

--- all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
+++ all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
@@ -2,30 +2,30 @@ class Solution {
 public:
     vector<vector<int>> v;
     
-    void getPaths(vector<vector<int>>& graph,int u,int n,vector<bool> &vis,vector<int> &path){
+    void getPaths(vector<vector<int>>& graph,int u,vector<int> &path,vector<bool> &isVis,int n){
+        
         if(u == n){
             path.push_back(u);
             v.push_back(path);
             path.pop_back();
         }else{
-            vis[u] = true;
+            isVis[u] = true;
             path.push_back(u);
             for(auto it : graph[u]){
-                if(!vis[it]){
-                    getPaths(graph,it,n,vis,path);
+                if(!isVis[it]){
+                    getPaths(graph,it,path,isVis,n);            
                 }
             }
             path.pop_back();
-            vis[u] = false;
+            isVis[u] = false;
         }
     }
     
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-        
         int n = graph.size();
-        vector<bool> vis(n,false);
         vector<int> path;
-        getPaths(graph,0,n-1,vis,path);
+        vector<bool> isVis(n,false);
+        getPaths(graph,0,path,isVis,n-1);
         return v;
     }
 };
