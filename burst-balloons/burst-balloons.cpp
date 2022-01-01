868084eb277b0621a4d4ccbb3556832458ed7334
Jayant Goel <jgoel92@gmail.com>
2022-01-01 05:43:14 +0530

    Time: 686 ms (34.52%), Space: 10.3 MB (27.29%) - LeetHub

--- burst-balloons/burst-balloons.cpp
+++ burst-balloons/burst-balloons.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int solve(vector<int> &nums,int i,int j,int l,int r){
-        if(i>j) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(i==j) return dp[i][j]= nums[i]*l*r;
-        int _max=0;
-        int temp;
-        for(int k=i;k<=j;k++){
-            int x= solve(nums,i,k-1,l,nums[k]);
-            int y=solve(nums,k+1,j,nums[k],r);
-            temp=x+y+nums[k]*l*r;
-            if(_max<temp){
-                 _max=temp;
+    int maxCoins(vector<int>& nums) {
+        int N = nums.size();
+        nums.insert(nums.begin(), 1);
+        nums.insert(nums.end(), 1);
+
+        // rangeValues[i][j] is the maximum # of coins that can be obtained
+        // by popping balloons only in the range [i,j]
+        vector<vector<int>> rangeValues(nums.size(), vector<int>(nums.size(), 0));
+
+        // build up from shorter ranges to longer ranges
+        for (int len = 1; len <= N; ++len) {
+            for (int start = 1; start <= N - len + 1; ++start) {
+                int end = start + len - 1;
+                // calculate the max # of coins that can be obtained by
+                // popping balloons only in the range [start,end].
+                // consider all possible choices of final balloon to pop
+                int bestCoins = 0;
+                for (int final = start; final <= end; ++final) {
+                    int coins = rangeValues[start][final-1] + rangeValues[final+1][end]; // coins from popping subranges
+                    coins += nums[start-1] * nums[final] * nums[end+1]; // coins from final pop
+                    if (coins > bestCoins) bestCoins = coins;
+                }
+                rangeValues[start][end] = bestCoins;
             }
         }
-        return dp[i][j]= _max;
-    }
-    int maxCoins(vector<int>& nums) {
-        int n= nums.size();
-        dp.assign(n,vector<int>(n,-1));
-        return solve(nums,0,nums.size()-1,1,1);
+        return rangeValues[1][N];
     }
 };
