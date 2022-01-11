0e316b76d1db4bd6e52b14c707ea4ed71d13f53e
Jayant Goel <jgoel92@gmail.com>
2022-01-11 20:10:08 +0530

    Time: 8 ms (53.68%), Space: 16.8 MB (43.41%) - LeetHub

---
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int sumRootToLeafHelper(TreeNode* rt, int cur_val) {
+        if(! rt) return 0;
+        
+        cur_val = (cur_val << 1) | rt->val;
+        
+        if(!rt->left && !rt->right)
+			return cur_val;
+			
+        return sumRootToLeafHelper(rt->left, cur_val) + sumRootToLeafHelper(rt->right, cur_val);
+    }
+    
+    int sumRootToLeaf(TreeNode* rt) {
+        return sumRootToLeafHelper(rt, 0);
+    }
+};
