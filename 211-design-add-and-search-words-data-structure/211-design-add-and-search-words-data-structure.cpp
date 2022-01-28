5ebbc07492ebe240503b5a664de57f48f00e32e6
Jayant Goel <jgoel92@gmail.com>
2022-01-28 09:11:37 +0530

    Time: 179 ms (23.18%), Space: 42.7 MB (79.15%) - LeetHub

---
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.cpp
@@ -0,0 +1,55 @@
+class Node{
+  public:
+    unordered_map<char,Node*> mp;
+    bool isEnd;
+    
+    Node(){
+        isEnd = false;
+    }
+};
+class WordDictionary {
+public:
+    Node *head;
+    WordDictionary() {
+        head = new Node();
+    }
+    
+    void addWord(string word) {
+        Node *temp = head;
+        for(auto it : word){
+            if(temp->mp[it] == nullptr){
+                temp->mp[it] = new Node();
+            }
+            temp = temp->mp[it];
+        }
+        temp->isEnd = true;
+    }
+    bool searchUtil(string &word,int x,Node *temp){
+        if(!temp){
+            return false;
+        }
+        if(x == word.size()){
+            return temp->isEnd;
+        }
+        if(word[x] != '.'){
+            if(temp->mp[word[x]] == nullptr){
+                return false;
+            }else{
+                return searchUtil(word,x+1,temp->mp[word[x]]);
+            }
+        }else{
+            for(auto it : temp->mp){
+                bool res = searchUtil(word,x+1,it.second);
+                if(res){
+                    return res;
+                }
+            }
+            return false;
+        }
+    }
+    bool search(string word) {
+        Node *temp = head;
+        
+        return searchUtil(word,0,temp);
+    }
+};
