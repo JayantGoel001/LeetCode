02a9f61c8f1fecdc1836a541f6545dd2fb2a4abb
Jayant Goel <jgoel92@gmail.com>
2022-01-31 12:14:05 +0530

    Time: 3 ms (76.45%), Space: 8.4 MB (13.95%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.cpp
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.cpp
@@ -4,7 +4,7 @@ public:
         int sum = 0;
         int size = arr.size();
         for(int i=0;i<size;i++){
-            sum += arr[i] * ceil(((size - i ) * (i + 1))/2.0);
+            sum += arr[i] * ceil(((size - i) * (i + 1))/2.0);
         }
         return sum;
     }
