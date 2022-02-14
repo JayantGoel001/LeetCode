02ccfdc9168387b62fef7f93bd70d840bd1a1929
Jayant Goel <jgoel92@gmail.com>
2022-02-14 13:40:36 +0530

    Time: 173 ms (58.98%), Space: 75.8 MB (36.38%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
     vector<vector<string>> v;
-    bool isPalindrome(string &s,int start,int end){
-        while(start <= end){
-            if(s[start] != s[end]){
-                return false;
-            }
-            start++;
-            end--;
-        }
-        return true;
-    }
+    vector<vector<bool>> dp;
     void generateComb(string &s,vector<string> &temp,int start){
         if(start == s.size()){
             v.push_back(temp);
         }else{
             for(int i=start;i<s.size();i++){
-                if(isPalindrome(s,start,i)){
+                if(s[start] == s[i] && (i - start <= 2 || dp[start + 1][i - 1])){
+                    dp[start][i] = true;
                     temp.push_back(s.substr(start,i - start + 1));
                     
                     generateComb(s,temp,i+1);
@@ -28,7 +20,14 @@ public:
     
     vector<vector<string>> partition(string s) {
         vector<string> temp;
+        int n = s.size();
         
+        for(int i=0;i<=n;i++){
+            dp.push_back(vector<bool>());
+            for(int j=0;j<=n;j++){
+                dp[i].push_back(false);
+            }
+        }
         generateComb(s,temp,0);
         
         return v;
