f2041a1fa5a249abf69f1932cf561a2fd4817b07
Jayant Goel <jgoel92@gmail.com>
2022-02-14 13:00:13 +0530

    Time: 269 ms (28.22%), Space: 75.8 MB (36.38%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -1,25 +1,36 @@
 class Solution {
 public:
-    vector<vector<string>> partition(string s) {
-        int len = s.size();
-        vector<vector<bool>> dp(len,vector<bool> (len,false));
-        vector<vector<string>> result;
-        vector<string> curr;
-        dfs(s,0,curr,result,dp);
-        return result;
-    }
-    
-    void dfs(string s,int start,vector<string> &cur,vector<vector<string>> &res,vector<vector<bool>> &dp){
-        if(s.length() == start){
-            res.push_back(cur);
+    vector<vector<string>> v;
+    bool isPalindrome(string &s,int start,int end){
+        while(start <= end){
+            if(s[start] != s[end]){
+                return false;
+            }
+            start++;
+            end--;
         }
-        for(int i = start;i<s.length();i++){
-            if(s[start] == s[i] && (i - start <= 2 || dp[start + 1][i - 1])){
-                dp[start][i] = true;
-                cur.push_back(s.substr(start,i - start + 1));
-                dfs(s,i+1,cur,res,dp);
-                cur.pop_back();
+        return true;
+    }
+    void generateComb(string &s,vector<string> &temp,int start){
+        if(start == s.size()){
+            v.push_back(temp);
+        }else{
+            for(int i=start;i<s.size();i++){
+                if(isPalindrome(s,start,i)){
+                    temp.push_back(s.substr(start,i - start + 1));
+                    
+                    generateComb(s,temp,i+1);
+                    temp.pop_back();
+                }
             }
         }
+    }
+    
+    vector<vector<string>> partition(string s) {
+        vector<string> temp;
+        
+        generateComb(s,temp,0);
+        
+        return v;
     }
 };
