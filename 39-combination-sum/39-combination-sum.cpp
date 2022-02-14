31a06ec4406f2963ba6424cd6a0edcc8548019d4
Jayant Goel <jgoel92@gmail.com>
2022-02-14 11:45:00 +0530

    Time: 9 ms (64.30%), Space: 11.1 MB (53.02%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> v;
+    
+    void generateComb(vector<int> &can,int target,vector<int> &temp,int x = 0){
+        if(target < 0){
+            return;
+        }
+        if(target == 0){
+            v.push_back(temp);
+        }else{
+            for(int i=x;i<can.size();i++){
+                target -= can[i];
+                temp.push_back(can[i]);
+                
+                generateComb(can,target,temp,i);
+                
+                target += can[i];
+                temp.pop_back();
+            }
+        }
+    }
+    
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> temp;
+        generateComb(candidates,target,temp);
+        return v;
+    }
+};
