2f3f25dbc449a8233ca70c58dc228a9bfb13afc5
Jayant Goel <jgoel92@gmail.com>
2022-02-25 15:42:21 +0530

    Time: 27 ms (71.42%), Space: 13.5 MB (81.34%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,28 +1,30 @@
 #define pii pair<int,int>
 class Solution {
 public:
-    bool isBipartite(vector<vector<int>>& graph) {
-        queue<pii> q;
+    vector<int> vis;
+    bool DFS(vector<vector<int>>& graph,int u,int color){
+        vis[u] = color;
         
+        for(auto it : graph[u]){
+            if(vis[it] == -1){
+                if(!DFS(graph, it, 1 - color)){
+                    return false;
+                }
+            }else if(vis[it] == color){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<int> vis(n,-1);
+        vis.resize(n,-1);
         
         for(int i=0;i<n;i++){
             if(vis[i] == -1){
-                q.push({i,0});
-                while(!q.empty()){
-                    pii top = q.front();
-                    q.pop();
-                    
-                    vis[top.first] = top.second;
-
-                    for(auto it : graph[top.first]){
-                        if(vis[it] == -1){
-                            q.push({it,1 - top.second});
-                        }else if(top.second == vis[it]){
-                            return false;
-                        }
-                    }
+                if(!DFS(graph,i,0)){
+                    return false;
                 }
             }
         }
