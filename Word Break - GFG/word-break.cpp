8bc1bb707b94a058233671be337dd226e86dfc65
Jayant Goel <jgoel92@gmail.com>
2022-02-26 19:40:05 +0530

    Added solution - LeetHub

---
+++ Word Break - GFG/word-break.cpp
@@ -0,0 +1,53 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+
+// A : given string to search
+// B : vector of available strings
+
+class Solution
+{
+public:
+    int wordBreak(string A, vector<string> &B) {
+        int n = A.size();
+        vector<bool> dp(n+1,false);
+        dp[0] = true;
+        
+        for(int i = 0;i<n;i++){
+            for(auto it : B){
+                if(dp[i] && it == A.substr(i, it.size())){
+                    dp[i + it.size()] = true;
+                }
+            }
+        }
+        return dp[n];
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<string> dict;
+        for(int i=0;i<n;i++){
+            string S;
+            cin>>S;
+            dict.push_back(S);
+        }
+        string line;
+        cin>>line;
+        Solution ob;
+        cout<<ob.wordBreak(line, dict)<<"\n";
+    }
+}
+  // } Driver Code Ends
