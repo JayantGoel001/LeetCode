866fe0351d20f93e709bf7c868c9ccc6fead7d07
Jayant Goel <jgoel92@gmail.com>
2022-03-03 09:30:14 +0530

    Time: 4 ms (49.49%), Space: 7.2 MB (88.02%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.cpp
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -6,7 +6,7 @@ public:
         if(n < 3){
             return 0;
         }
-        vector<int> dp(n + 1,0);
+        vector<int> dp(n,0);
         
         for(int i=2;i<n;i++){
             if(nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]){
