0699b98895fd2103618a6a20d6d5c76bc68733ec
Jayant Goel <jgoel92@gmail.com>
2022-03-04 01:26:39 +0530

    Time: 7 ms (45.94%), Space: 9.6 MB (87.68%) - LeetHub

--- 717-1-bit-and-2-bit-characters/717-1-bit-and-2-bit-characters.cpp
+++ 717-1-bit-and-2-bit-characters/717-1-bit-and-2-bit-characters.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
-        bool isOneBitCharacter(vector<int>& bits) {
-        int count_1 = 0;
-        for (int i = (int) bits.size() - 2; i >= 0 && bits[i] == 1; i--) count_1++;
-        return (count_1 % 2 == 0);
+    bool isOneBitCharacter(vector<int>& bits) {
+        int count = 0;
+        
+        for(int i=bits.size()-2;i>=0 && bits[i] == 1;i--){
+            count++;
+        }
+        return count%2 == 0;
     }
-
 };
