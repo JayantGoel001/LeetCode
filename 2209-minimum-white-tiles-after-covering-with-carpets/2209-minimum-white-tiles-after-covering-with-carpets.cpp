cafce7eb05b3a406a79d7b066fa5de3168a48748
Jayant Goel <jgoel92@gmail.com>
2022-03-19 23:02:05 +0530

    Time: 1611 ms (11.11%), Space: 254.4 MB (11.11%) - LeetHub

--- 2209-minimum-white-tiles-after-covering-with-carpets/2209-minimum-white-tiles-after-covering-with-carpets.cpp
+++ 2209-minimum-white-tiles-after-covering-with-carpets/2209-minimum-white-tiles-after-covering-with-carpets.cpp
@@ -1,35 +1,15 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    vector<int> whiteSum;
-    
-    int getCovered(int num,int len,int floorLen,int idx = 0){
-        if(num <= 0){
-            return 0;
+    int minimumWhiteTiles(string s, int nc, int l) {
+        int n = s.size();
+        vector<vector<int>> dp(n + 1, vector<int>(nc + 1));
+        for (int i = 1; i <= n; ++i) {
+            for (int k = 0; k <= nc; ++k) {
+                int jump = dp[i - 1][k] + s[i - 1] - '0';
+                int cover = k > 0 ? dp[max(i - l, 0)][k - 1] : 1000;
+                dp[i][k] = min(cover, jump);
+            }
         }
-        if(floorLen - idx <= len){
-            return whiteSum[floorLen - 1] - (idx == 0 ? 0 : whiteSum[idx - 1]);
-        }
-        if(dp[idx][num] == -1){
-            dp[idx][num] = max(
-                whiteSum[idx + len - 1] - (idx == 0 ? 0 : whiteSum[idx - 1]) + getCovered(num - 1,len,floorLen,idx + len),
-                getCovered(num,len,floorLen,idx + 1)
-            );
-        }
-        return dp[idx][num];
-    }
-    int minimumWhiteTiles(string floor, int numCarpets, int carpetLen) {
-        int white = 0;
-        for(auto it : floor){
-            white += (it == '1');
-        }
-        whiteSum.resize(floor.size(), 0);
-        whiteSum[0] = (floor[0] == '1' ? 1 : 0);
-        
-        for(int i=1;i<floor.size();i++){
-            whiteSum[i] = whiteSum[i - 1] + (floor[i] == '1' ? 1 : 0);    
-        }
-        dp.resize(floor.size(), vector<int> (numCarpets + 1, -1));
-        return white - getCovered(numCarpets,carpetLen,floor.size());
+        return dp[n][nc];
     }
 };
