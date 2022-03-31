6c6398e3d38065634e2937cb3c837e9750a0a52b
Jayant Goel <jgoel92@gmail.com>
2022-03-31 16:21:13 +0530

    Time: 32 ms (50.74%), Space: 16.8 MB (85.68%) - LeetHub

---
+++ 622-design-circular-queue/622-design-circular-queue.cpp
@@ -0,0 +1,34 @@
+class MyCircularQueue {
+public:
+    MyCircularQueue(int k) {
+        data.resize(k);
+        maxSize = k;
+    }
+    bool enQueue(int val) {
+        if (isFull()) return false;
+        tail = (tail + 1) % maxSize;
+        data[tail] = val;
+        return true;
+    }
+    bool deQueue() {
+        if (isEmpty()) return false;
+        if (head == tail) head = 0, tail = -1;
+        else head = (head + 1) % maxSize;
+        return true;
+    }
+    int Front() {
+        return isEmpty() ? -1 : data[head];
+    }
+    int Rear() {
+        return isEmpty() ? -1 : data[tail];
+    }
+    bool isEmpty() {
+        return tail == -1;
+    }
+    bool isFull() {
+        return !isEmpty() && (tail + 1) % maxSize == head;
+    }
+private:
+    int maxSize, head = 0, tail = -1;
+    vector<int> data;
+};
