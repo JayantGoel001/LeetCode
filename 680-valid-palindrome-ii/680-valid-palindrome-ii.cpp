536474a94fd8a90d1b90dfc97d66e5f59df87590
Jayant Goel <jgoel92@gmail.com>
2022-04-02 10:16:29 +0530

    Time: 60 ms (80.88%), Space: 24.1 MB (31.50%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool validPalindrome(string s) {
+        int i = 0;
+        int j = s.size() - 1;
+        
+        while(i <= j){
+            if(s[i] == s[j]){
+                i++;
+                j--;
+            }
+            else return isPalindrome(s, i + 1, j) || isPalindrome(s, i, j - 1);
+        }
+        return true;
+    }
+    bool isPalindrome(string s, int i, int j){
+        while(i <= j){
+            if(s[i] == s[j]){
+                i++;
+                j--;
+            }
+            else return false;
+        }
+        return true;
+    }
+};
