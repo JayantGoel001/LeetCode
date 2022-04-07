c632c622a35d7c04520a1f33824b094be140e7f5
Jayant Goel <jgoel92@gmail.com>
2022-04-07 20:01:22 +0530

    Time: 255 ms (79.67%), Space: 0B (100.00%) - LeetHub

---
+++ 595-big-countries/595-big-countries.sql
@@ -0,0 +1,16 @@
+SELECT
+    name, population, area
+FROM
+    world
+WHERE
+    area >= 3000000
+
+UNION
+
+SELECT
+    name, population, area
+FROM
+    world
+WHERE
+    population >= 25000000
+;
