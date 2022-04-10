1d4d73edfbbf8f4cbf4b8b7032dbab0f284f127b
Jayant Goel <jgoel92@gmail.com>
2022-04-10 09:29:56 +0530

    Time: 458 ms (47.13%), Space: 0B (100.00%) - LeetHub

---
+++ 608-tree-node/608-tree-node.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT
+    atree.id,
+    IF(ISNULL(atree.p_id),
+        'Root',
+        IF(atree.id IN (SELECT p_id FROM tree), 'Inner','Leaf')) Type
+FROM
+    tree atree
+ORDER BY atree.id
