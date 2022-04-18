a4636711626b78b26c5c03a042efdb23a8ed37e2
Jayant Goel <jgoel92@gmail.com>
2022-04-18 11:41:50 +0530

    Time: 23 ms (56.34%), Space: 18.7 MB (47.36%) - LeetHub

---
+++ 275-h-index-ii/275-h-index-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int left=0, len = citations.size(), right= len-1,  mid;
+        while(left<=right)
+        {
+            mid=left+ (right-left)/2;
+            if(citations[mid] >= (len-mid)) right = mid - 1;
+            else left = mid + 1;
+        }
+        return len - left;
+    }
+};
