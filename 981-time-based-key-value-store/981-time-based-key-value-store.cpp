51079d7e9249a40d5ee24f152b53b79ad8926f07
Jayant Goel <jgoel92@gmail.com>
2022-04-26 08:00:39 +0530

    Time: 370 ms (66.27%), Space: 128.1 MB (86.05%) - LeetHub

---
+++ 981-time-based-key-value-store/981-time-based-key-value-store.cpp
@@ -0,0 +1,12 @@
+class TimeMap {
+public:
+    unordered_map<string, vector<pair<int, string>>> m;
+    void set(string key, string value, int timestamp) {
+      m[key].push_back({ timestamp, value });
+    }
+    string get(string key, int timestamp) {
+      auto it = upper_bound(begin(m[key]), end(m[key]), pair<int, string>(timestamp, ""), [](
+        const pair<int, string>& a, const pair<int, string>& b) { return a.first < b.first; });
+      return it == m[key].begin() ? "" : prev(it)->second;
+    }
+};
