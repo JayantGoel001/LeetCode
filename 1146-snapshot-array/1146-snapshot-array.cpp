41fb6d4dda6bf0bdb07f87837f2bfff507ed83df
Jayant Goel <jgoel92@gmail.com>
2022-04-28 10:39:22 +0530

    Time: 263 ms (73.51%), Space: 87.5 MB (88.15%) - LeetHub

---
+++ 1146-snapshot-array/1146-snapshot-array.cpp
@@ -0,0 +1,16 @@
+class SnapshotArray {
+public:
+    unordered_map<int, map<int, int>> a;
+    int cur_snap = 0;
+    SnapshotArray(int length) {}
+    int snap() { 
+        return cur_snap++; 
+    }
+    void set(int index, int val) { 
+        a[index][cur_snap] = val; 
+    }
+    int get(int index, int snap_id) {
+        auto it = a[index].upper_bound(snap_id);
+        return it == begin(a[index]) ? 0 : prev(it)->second;
+    }
+};
