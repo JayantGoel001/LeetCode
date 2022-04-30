a83524c8b3aa904b086fae838bb23b0d3f130066
Jayant Goel <jgoel92@gmail.com>
2022-04-30 10:23:14 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (32.23%) - LeetHub

---
+++ 1201-ugly-number-iii/1201-ugly-number-iii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:    
+   int nthUglyNumber(int k, int A, int B, int C) {
+        int lo = 1, hi = 2 * (int) 1e9;
+        long a = long(A), b = long(B), c = long(C);
+        long ab = a * b / __gcd(a, b);
+        long bc = b * c / __gcd(b, c);
+        long ac = a * c / __gcd(a, c);
+        long abc = a * bc / __gcd(a, bc);
+        while(lo < hi) {
+            int mid = lo + (hi - lo)/2;
+            int cnt = mid/a + mid/b + mid/c - mid/ab - mid/bc - mid/ac + mid/abc;
+            if(cnt < k) 
+                lo = mid + 1;
+            else
+			   //the condition: F(N) >= k
+                hi = mid;
+        }
+        return lo;
+    }
+};
