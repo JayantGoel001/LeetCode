0f11e613627ffd262780497d1fe1215ff1617ed0
Jayant Goel <jgoel92@gmail.com>
2022-05-01 09:37:07 +0530

    Time: 532 ms (33.33%), Space: 171.7 MB (33.33%) - LeetHub

--- 2261-k-divisible-elements-subarrays/2261-k-divisible-elements-subarrays.cpp
+++ 2261-k-divisible-elements-subarrays/2261-k-divisible-elements-subarrays.cpp
@@ -7,19 +7,18 @@ public:
         for(int i=0;i<nums.size();i++){
             int countK = 0;
             int j;
+            string str = "";
+            
             for(j=i;j<nums.size();j++){
                 countK += (nums[j]%p == 0 ? 1 : 0);
                 if(countK > k){
                     break;
                 }
-            }
-            count += (j - i);
-            int w = i;
-            string str = "";
-            while(w < j){
-                str += (nums[w++] + '0');
+                
+                str += (nums[j] + '0');
                 mp[str]++;
             }
+            count += (j - i);
         }
         for(auto it : mp){
             count -= (it.second-1);
