5404aa7e6b3d3eed5a1b5799e29f3969fd8d3cb1
Jayant Goel <jgoel92@gmail.com>
2022-05-01 09:41:07 +0530

    Time: 422 ms (40.00%), Space: 115.1 MB (40.00%) - LeetHub

--- 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.cpp
+++ 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     int minimumCardPickup(vector<int>& cards) {
-        unordered_map<int,vector<int>> mp;
-        for(int i=0;i<cards.size();i++){
-            mp[cards[i]].push_back(i);
-        }
+        unordered_map<int,int> mp;
         int mini = INT_MAX;
-        for(auto it : mp){
-            if(it.second.size() >= 2){
-                for(int i = 0;i < it.second.size() - 1;i++){
-                    mini = min(mini, it.second[i + 1] - it.second[i] + 1);
-                }
-            }
+        for(int i=0;i<cards.size();i++){
+            if(mp[cards[i]] != 0){
+                mini = min(mini,i + 1 - mp[cards[i]] + 1);
+            }   
+            mp[cards[i]] = i + 1;
         }
         return mini == INT_MAX ? -1 : mini;
     }
