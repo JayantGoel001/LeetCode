e3f369a8d100c8adbbf59223308aadcf16afaba6
Jayant Goel <jgoel92@gmail.com>
2022-06-15 11:13:43 +0530

    Time: 90 ms (82.33%), Space: 17.2 MB (51.94%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    static bool compare(const string &s1, const string &s2) {
+        return s1.length() < s2.length();
+    }
+
+    int longestStrChain(vector<string>& words) {
+        sort(words.begin(), words.end(), compare);
+        unordered_map<string, int> dp;
+        int res = 0;
+        for (string w : words) {
+            for (int i = 0; i < w.length(); i++) {
+                string pre = w.substr(0, i) + w.substr(i + 1);
+                dp[w] = max(dp[w], dp.find(pre) == dp.end() ? 1 : dp[pre] + 1);
+            }
+            res = max(res, dp[w]);
+        }
+        return res;
+    }
+};
