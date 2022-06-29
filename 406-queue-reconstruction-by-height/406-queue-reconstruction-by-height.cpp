42309f69f1f36ace9ef450e7beb528c25a2c526e
Jayant Goel <jgoel92@gmail.com>
2022-06-29 07:00:12 +0530

    Time: 42 ms (93.24%), Space: 11.6 MB (96.65%) - LeetHub

---
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> BIT;
+    int n;
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+        n = people.size();
+        BIT = vector<int>(n+1, 0); //BIT[i+1] recorded the res[i] information because BIT[0] is not used.
+        for(int i = 2; i <= n; i++) update(i, 1);  // BIT[1] is the 0th empty position, so we didn't add 1
+        sort(people.begin(), people.end(), cmp);
+        vector<vector<int>> res(n, vector<int>());
+        for(int i = 0; i < n; i++){
+            int l=0, r=n;
+            while(l<r){
+                int mid=l+(r-l)/2;
+                if(getsum(mid+1)<people[i][1]) l=mid+1; // we need get the index mid empty information, but actually it's stored in BIT[mid+1]
+                else r=mid;
+            }
+            res[l]=people[i];
+            update(l+1, -1);
+        }
+        return res;
+    }
+    void update(int x, int v){
+        for(int i = x; i <= n; i+=(i&-i)){
+            BIT[i]+=v;
+        }
+    }
+    int getsum(int x){
+        int sum=0;
+        for(int i = x; i > 0; i-=(i&-i)){
+            sum += BIT[i];
+        }
+        return sum;
+    }
+    static bool cmp(vector<int>& p1, vector<int>& p2){
+        if(p1[0]!=p2[0]) return p1[0]<p2[0];
+        else return p1[1]>p2[1];
+    }
+};
