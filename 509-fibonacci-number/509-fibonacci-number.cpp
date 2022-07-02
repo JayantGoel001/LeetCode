99b7b0ad0735748b569bde85b9e8515c390eb289
Jayant Goel <jgoel92@gmail.com>
2022-07-02 13:09:49 +0530

    Time: 3 ms (60.91%), Space: 6 MB (41.46%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    double power(double x,int n){
+        if(n == 0){
+            return 1;
+        }
+        if(n%2 == 0){
+            return power(x * x, n/2);
+        }else{
+            return x * power(x * x, (n - 1)/2);
+        }
+    }
+    int fib(int n) {
+        return round(power((sqrt(5) + 1.0)/2.0, n)/sqrt(5));
+    }
+};
