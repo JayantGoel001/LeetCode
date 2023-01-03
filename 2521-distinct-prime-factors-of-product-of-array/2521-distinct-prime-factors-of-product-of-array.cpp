0abd722d6c94c9c6a716516df38e11ad017669a2
Jayant Goel <jgoel92@gmail.com>
2023-01-03 18:41:21 +0530

    Time: 103 ms (42.96%), Space: 19.1 MB (24.61%) - LeetHub

---
+++ 2521-distinct-prime-factors-of-product-of-array/2521-distinct-prime-factors-of-product-of-array.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int distinctPrimeFactors(vector<int>& nums) {
+        set<int> st;
+        
+        vector<bool> v(10001, true);
+        
+        v[1] = false;
+        for(int i=4;i<=10000;i += 2){
+            v[i] = false;
+        }
+        for(int i = 3; i * i <= 10000; i+=2){
+            for(int j = i * i; j <= 10000; j+=i){
+                v[j] = false;
+            }
+        }
+        
+        for(auto it : nums){
+            int count = 0;
+            
+            for(int i=1;i*i<=it;i++){
+                if(it % i == 0){
+                    if(v[i]){
+                        st.insert(i);
+                    }
+                    if(it % (it/i) == 0 && v[it/i]){
+                        st.insert(it/i);
+                    }
+                }
+            }
+        }
+        return st.size();
+    }
+};
