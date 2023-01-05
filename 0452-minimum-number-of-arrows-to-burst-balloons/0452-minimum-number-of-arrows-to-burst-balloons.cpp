5744c5250f0265a5f215a18542eb9eb10a2d706a
Jayant Goel <jgoel92@gmail.com>
2023-01-05 22:45:31 +0530

    Time: 1088 ms (24.09%), Space: 102.3 MB (8.96%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
+    static bool cmp(vector<int>& a, vector<int>& b) {return a[1] < b[1];}
     int findMinArrowShots(vector<vector<int>>& points) {
-        sort(points.begin(), points.end());
+        sort(points.begin(), points.end(), cmp);
         
         int prev = points[0][1];
         int count = 0;
         
         for(auto it : points){
-            if(prev >= it[0]){
-                prev = min(it[1], prev);
-            }else{
+            if(prev < it[0]){
                 count++;
-                prev = it[1];
+                prev = max(prev, it[1]);
             }
         }
         return count+1;
