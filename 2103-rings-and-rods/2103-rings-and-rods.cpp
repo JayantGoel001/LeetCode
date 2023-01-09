df270c4fefe9482909463c2519670d40d764770f
Jayant Goel <jgoel92@gmail.com>
2023-01-09 23:40:57 +0530

    Time: 5 ms (16.47%), Space: 6.5 MB (27.15%) - LeetHub

---
+++ 2103-rings-and-rods/2103-rings-and-rods.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countPoints(string rings) {
+        vector<set<char>> v(10);
+        
+        for(int i=1;i<rings.size();i+=2){
+            v[rings[i] - '0'].insert(rings[i-1]);
+        }
+        
+        int count = 0;
+        for(auto it : v){
+            count += it.size() == 3;
+        }
+        return count;
+    }
+};
