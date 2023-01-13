b0409b0a99981de9b53d518b6eadb7b44cee33b3
Jayant Goel <jgoel92@gmail.com>
2023-01-13 19:57:21 +0530

    Time: 7 ms (74.86%), Space: 9 MB (55.37%) - LeetHub

---
+++ 0942-di-string-match/0942-di-string-match.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> diStringMatch(string s) {
+        int i = 0;
+        int d = s.size();
+        vector<int> v;
+        for(auto it : s){
+            if(it == 'I'){
+                v.push_back(i++);
+            }else{
+                v.push_back(d--);
+            }
+        }
+        v.push_back(s[s.size() - 1] == 'I' ? i : d);
+        return v;
+    }
+};
