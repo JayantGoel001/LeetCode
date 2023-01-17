e0bb1cf3cbaadb51766f50803f5b10588e5fc2bb
Jayant Goel <jgoel92@gmail.com>
2023-01-17 21:47:47 +0530

    Time: 26 ms (80.51%), Space: 11.2 MB (52.66%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
     int minFlipsMonoIncr(string s) {
-        int zeros = 0;
+        int ans = 0;
+        int ones = 0;
         
         for(auto it : s){
-            zeros += it == '0';
+            if(it == '0'){
+                ans = min(ones, ans + 1);
+            }else{
+                ones++;
+            }
         }
-        
-        int _z = 0;
-        int _o = 0;
-        
-        int mini = INT_MAX;
-        for(int i=0;i<s.size();i++){
-            mini = min(mini, zeros - _z + _o);
-            _z += s[i] == '0';
-            _o += s[i] == '1';
-        }
-        mini = min(mini, zeros - _z + _o);
-        return mini;
+        return ans;
     }
 };
