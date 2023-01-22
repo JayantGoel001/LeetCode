34ff05be0292a016ac1f4f365ffa542981cf4489
Jayant Goel <jgoel92@gmail.com>
2023-01-22 11:09:38 +0530

    Time: 22 ms (67.74%), Space: 14.7 MB (65.04%) - LeetHub

--- 2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.cpp
+++ 2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.cpp
@@ -1,26 +1,15 @@
-#define pis pair<int, string>
 class Solution {
 public:
     string kthDistinct(vector<string>& arr, int k) {
-        unordered_map<string,int> mp;
-        for(int i=0;i<arr.size();i++){
-            if(mp[arr[i]] != 0){
-                mp[arr[i]] = -1;
-            }else{
-                mp[arr[i]] = i + 1;
+        unordered_map<string, int> mp;
+        for(auto it : arr){
+            mp[it]++;
+        }
+        for(auto it : arr){
+            if(mp[it] == 1 && --k == 0){
+                return it;
             }
         }
-        priority_queue<pis> pq;
-        
-        for(auto it : mp){
-            if(it.second != -1){
-                pq.push({it.second, it.first});
-                
-                if(pq.size() > k){
-                    pq.pop();
-                }
-            }
-        }
-        return pq.size() < k ? "" : pq.top().second;
+        return "";
     }
 };
