a1d6ff8e37598fdb71af5d8ae2f93e15ec44b770
Jayant Goel <jgoel92@gmail.com>
2023-01-23 20:02:20 +0530

    Time: 8 ms (30.12%), Space: 6.7 MB (53.63%) - LeetHub

--- 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.cpp
+++ 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
     vector<int> shortestToChar(string s, char c) {
-        int first = INT_MAX;
-        int second = 0;
-        while(s[second] != c){
-            second++;
-        }
-        vector<int> v;
+        int prev = INT_MAX;
+        
+        vector<int> v(s.size(), 0);
         for(int i=0;i<s.size();i++){
             if(s[i] == c){
-                int start = i + 1;
-                while(start < s.size() && s[start] != c){
-                    start++;
-                }
-                first = i;
-                if(start != s.size()){
-                    second = start;
-                }else{
-                    second = INT_MAX;
-                }
+                prev = i;
             }
-            v.push_back(min(abs(i - first), abs(second - i)));
+            v[i] = abs(i - prev);
+        }
+        prev = INT_MAX;
+        for(int i=s.size() - 1;i >= 0;i--){
+            if(s[i] == c){
+                prev = i;
+            }
+            v[i] = min(v[i], abs(i - prev));
         }
         return v;
     }
