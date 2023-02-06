499c93a23522ae15e31e46d1320ca262681fadd6
Jayant Goel <jgoel92@gmail.com>
2023-02-06 20:20:02 +0530

    Time: 7 ms (79.26%), Space: 6.6 MB (12.94%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
@@ -1,25 +1,14 @@
 class Solution {
 public:
-    bool checkSqrt(int c, int low, int high){
-        long long mid = 0;
-        while(low <= high){
-            mid = (long long)low + (high - low)/2;
-            
-            if(mid * mid == c){
-                return true;
-            }else if(mid * mid < c){
-                low = mid + 1;
-            }else{
-                high = mid - 1;
-            }
-        }
-        return mid * mid == c;
-    }
     int countTriples(int n) {
+        vector<bool> v(n * n + 1, false);
+        for(int i=1;i <= n;i++){
+            v[i * i] = true;
+        }
         int count = 0;
         for(int i=1;i<n;i++){
             for(int j=1;j<n;j++){
-                count += checkSqrt(i * i + j * j, max(i, j), n);
+                count += (i * i + j * j <= n * n && v[i * i + j * j]);
             }
         }
         return count;
