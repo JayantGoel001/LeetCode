cc11f1c973e2e48283f7c932ebdabd92690359f1
Jayant Goel <jgoel92@gmail.com>
2023-02-28 15:00:10 +0530

    Time: 7 ms (82.02%), Space: 6 MB (71.25%) - LeetHub

--- 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.cpp
+++ 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.cpp
@@ -4,16 +4,6 @@ public:
         return n == 2 || n == 3 || n == 5 || n == 7 || n == 11 || n == 13 || n == 17 || n == 19;
     }
     int countPrimeSetBits(int left, int right) {
-        unordered_set<int> st;
-        st.insert(2);
-        st.insert(3);
-        st.insert(5);
-        st.insert(7);
-        st.insert(11);
-        st.insert(13);
-        st.insert(17);
-        st.insert(19);
-        
         int count = 0;
         for(int i=left;i<=right;i++){
             int num = i;
