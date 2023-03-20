8229da61b803fa533110b76a64f50d13d3ad46a6
Jayant Goel <jgoel92@gmail.com>
2023-03-20 17:05:47 +0530

    Time: 20 ms (53.38%), Space: 20.2 MB (76.11%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -5,8 +5,11 @@ public:
             if(flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && (i == flowerbed.size() - 1 || flowerbed[i + 1] == 0)){
                 flowerbed[i] = 1;
                 n--;
+                if(n <= 0){
+                    return true;
+                }
             }
         }
-        return n <= 0;
+        return n<=0;
     }
 };
