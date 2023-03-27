3f169a8be31e3ce14f8ed9c26808d90d2b4d7705
Jayant Goel <jgoel92@gmail.com>
2023-03-27 13:49:18 +0530

    Time: 5 ms (93.57%), Space: 9.6 MB (98.74%) - LeetHub

---
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        for(int i=0;i<n;i++){
+            for(int j = 0;j < m;j++){
+                int x = min(i == 0? INT_MAX : grid[i - 1][j], j == 0 ? INT_MAX : grid[i][j - 1]);
+                if(x != INT_MAX){
+                    grid[i][j] += x;
+                }
+            }
+        }
+        return grid[n - 1][m - 1];
+    }
+};
