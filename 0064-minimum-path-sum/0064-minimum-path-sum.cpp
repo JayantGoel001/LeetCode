176dbc0a3f3807c8ac4ddcf90f8e909219f99f08
Jayant Goel <jgoel92@gmail.com>
2023-03-27 13:58:27 +0530

    Time: 15 ms (20.14%), Space: 9.7 MB (92.52%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -4,7 +4,7 @@ public:
         int n = grid.size();
         int m = grid[0].size();
         
-        for(int i=0;i<n;i++){
+        for(int i = 0;i < n;i++){
             for(int j = 0;j < m;j++){
                 int x = min(i == 0? INT_MAX : grid[i - 1][j], j == 0 ? INT_MAX : grid[i][j - 1]);
                 if(x != INT_MAX){
