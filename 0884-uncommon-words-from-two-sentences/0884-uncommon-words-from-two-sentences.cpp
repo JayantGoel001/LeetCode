79e21779b7774d694701320e4d67f4682d90eb38
Jayant Goel <jgoel92@gmail.com>
2023-04-10 14:41:10 +0530

    Time: 2 ms (59.20%), Space: 6.9 MB (59.79%) - LeetHub

--- 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.cpp
+++ 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.cpp
@@ -1,9 +1,19 @@
 class Solution {
 public:
-    unordered_map<string,int> getWords(string str){
+    vector<string> uncommonFromSentences(string s1, string s2) {
         unordered_map<string,int> mp;
         string s = "";
-        for(auto it : str){
+        for(auto it : s1){
+            if(it == ' '){
+                mp[s]++;
+                s = "";
+            }else{
+                s += it;
+            }
+        }
+        mp[s]++;
+        s = "";
+        for(auto it : s2){
             if(it == ' '){
                 mp[s]++;
                 s = "";
@@ -13,20 +23,9 @@ public:
         }
         mp[s]++;
         
-        return mp;
-    }
-    vector<string> uncommonFromSentences(string s1, string s2) {
-        unordered_map<string,int> st1 = getWords(s1);
-        unordered_map<string,int> st2 = getWords(s2);
-        
         vector<string> v;
-        for(auto it : st1){
-            if(it.second == 1 && st2[it.first] == 0){
-                v.push_back(it.first);
-            }
-        }
-        for(auto it : st2){
-            if(it.second == 1 && st1[it.first] == 0){
+        for(auto it : mp){
+            if(it.second == 1){
                 v.push_back(it.first);
             }
         }
