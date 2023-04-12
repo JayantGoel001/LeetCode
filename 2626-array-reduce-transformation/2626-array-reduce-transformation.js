baf078796d57cab07c4260f44a34165a6b465946
Jayant Goel <jgoel92@gmail.com>
2023-04-12 19:20:37 +0530

    Time: 58 ms (100.00%), Space: 42.5 MB (100.00%) - LeetHub

---
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    let res = init;
+    
+    for(let i = 0;i < nums.length;i++){
+        res = fn(res, nums[i]);
+    }
+    return res;
+};
