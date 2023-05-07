fd277ee3902d75cf71007e5cb24756ff81e07863
Jayant Goel <jgoel92@gmail.com>
2023-05-07 11:41:55 +0530

    Time: 71 ms (38.48%), Space: 44.7 MB (54.74%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,22 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let initial = init;
+    return {
+        increment: () => ++init, 
+        decrement: () => --init,
+        reset: () => {
+            init = initial
+            return init;
+        }
+    };
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
