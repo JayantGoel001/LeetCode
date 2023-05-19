91317d9024d360558e07c2d879cdd452235c4e4e
Jayant Goel <jgoel92@gmail.com>
2023-05-19 11:00:26 +0530

    Time: 63 ms (44.59%), Space: 42.2 MB (22.16%) - LeetHub

---
+++ 2627-debounce/2627-debounce.js
@@ -0,0 +1,10 @@
+var debounce = function(fn, t) {
+  let timeout;
+  return function(...args) {
+    clearTimeout(timeout);
+      
+    timeout = setTimeout(() => {
+      fn(...args)
+    }, t);
+  };
+};
