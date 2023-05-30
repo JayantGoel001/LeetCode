a33d55dab03dbbc5eaeb357764d0ec320cfe9d27
Jayant Goel <jgoel92@gmail.com>
2023-05-30 11:57:02 +0530

    Time: 56 ms (78.86%), Space: 42.4 MB (29.22%) - LeetHub

---
+++ 2693-call-function-with-custom-context/2693-call-function-with-custom-context.js
@@ -0,0 +1,8 @@
+/**
+ * @param {Object} context
+ * @param {any[]} args
+ * @return {any}
+ */
+Function.prototype.callPolyfill = function(context, ...args) {
+    return this.bind(context)(...args);
+}
