6503211546e64e7637f968088db4d264233e549c
Jayant Goel <jgoel92@gmail.com>
2023-06-02 19:16:58 +0530

    Time: 49 ms (92.71%), Space: 42.3 MB (28.34%) - LeetHub

---
+++ 2648-generate-fibonacci-sequence/2648-generate-fibonacci-sequence.js
@@ -0,0 +1,11 @@
+var fibGenerator = function*() {
+    let prev1 = 0;
+    let prev2 = 1;
+
+    while(true) {
+        yield prev1;
+        const temp = prev1;
+        prev1 = prev2;
+        prev2 += temp;
+    }
+};
