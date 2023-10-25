f73408f24fd8d623b1a71ae5cbf01502f4207f60
Jayant Goel <jgoel92@gmail.com>
2023-10-25 20:02:25 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (12.22%) - LeetHub

---
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int recursion(int n, int k) {
+        // First row will only have one symbol '0'.
+        if (n == 1) {
+            return 0;
+        }
+
+        int totalElements = pow(2, (n - 1));
+        int halfElements = totalElements / 2;
+
+        // If the target is present in the right half, we switch to the respective left half symbol.
+        if (k > halfElements) {
+            return 1 - kthGrammar(n, k - halfElements);
+        }
+
+        // Otherwise, we switch to the previous row.
+        return recursion(n - 1, k);
+    }
+
+    int kthGrammar(int n, int k) {
+        return recursion(n, k);
+    }
+};
