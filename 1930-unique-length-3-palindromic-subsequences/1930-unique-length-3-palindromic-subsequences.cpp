8b066c2cab1be3fa3d23ed059167b24a6868cb76
Jayant Goel <jgoel92@gmail.com>
2023-11-14 06:47:01 +0530

    Time: 274 ms (62.10%), Space: 13.3 MB (80.37%) - LeetHub

---
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int countPalindromicSubsequence(string s) {
+        vector<int> first = vector(26, -1);
+        vector<int> last = vector(26, -1);
+        
+        for (int i = 0; i < s.size(); i++) {
+            int curr = s[i] - 'a';
+            if (first[curr] == - 1) {
+                first[curr] = i;
+            }
+            
+            last[curr] = i;
+        }
+        
+        int ans = 0;
+        for (int i = 0; i < 26; i++) {
+            if (first[i] == -1) {
+                continue;
+            }
+            
+            unordered_set<char> between;
+            for (int j = first[i] + 1; j < last[i]; j++) {
+                between.insert(s[j]);
+            }
+            
+            ans += between.size();
+        }
+        
+        return ans;
+    }
+};
