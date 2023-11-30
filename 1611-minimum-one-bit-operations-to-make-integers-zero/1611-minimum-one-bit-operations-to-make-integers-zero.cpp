249110ea7748c8b0600a5be38365ea2229ea74e7
Jayant Goel <jgoel92@gmail.com>
2023-11-30 20:24:42 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (52.61%) - LeetHub

---
+++ 1611-minimum-one-bit-operations-to-make-integers-zero/1611-minimum-one-bit-operations-to-make-integers-zero.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minimumOneBitOperations(int n) {
+        int ans = n;
+        ans ^= ans >> 16;
+        ans ^= ans >> 8;
+        ans ^= ans >> 4;
+        ans ^= ans >> 2;
+        ans ^= ans >> 1;
+        return ans;
+    }
+};
