d1b341c18b58a63dd1ed3df9800d36428ec5a280
Jayant Goel <jgoel92@gmail.com>
2024-01-29 09:43:34 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (11.04%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -36,12 +36,3 @@ public:
         return st1.empty() && st2.empty();
     }
 };
-
-/**
- * Your MyQueue object will be instantiated and called as such:
- * MyQueue* obj = new MyQueue();
- * obj->push(x);
- * int param_2 = obj->pop();
- * int param_3 = obj->peek();
- * bool param_4 = obj->empty();
- */
