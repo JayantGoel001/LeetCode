4e82185193b449d0489aa5b92a9b27dabd169dd5
Jayant Goel <jgoel92@gmail.com>
2024-03-13 14:07:45 +0530

    Time: 3 ms (51.39%), Space: 7.2 MB (41.86%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -1,17 +1,10 @@
 class Solution {
 public:
     int pivotInteger(int n) {
-        int rsum = (n * (n + 1))/2;
+        int res = n * (n + 1) / 2;
         
-        int lsum = 0;
-        for(int i=1;i<=n;i++){
-            lsum += i;
-            
-            if(lsum == rsum){
-                return i;
-            }
-            rsum -= i;
-        }
-        return -1;
+        int x = sqrt(res);
+        
+        return x * x == res ? x : -1;
     }
 };
