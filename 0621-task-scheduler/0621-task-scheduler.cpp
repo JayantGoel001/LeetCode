5b3c05028c8ae5af04ca4455995819d24f2bea8b
Jayant Goel <jgoel92@gmail.com>
2024-03-19 12:43:17 +0530

    Time: 45 ms (90.28%), Space: 38 MB (80.77%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        unordered_map<char,int>mp;
+        vector<int> v(26, 0);
         int count = 0;
-        for(auto e : tasks)
-        {
-            mp[e]++;
-            count = max(count, mp[e]);
+        for(auto e : tasks){
+            v[e - 'A']++;
+            count = max(count, v[e - 'A']);
         }
         
-        int ans = (count-1)*(n+1);
-        for(auto e : mp) if(e.second == count) ans++;
+        int ans = (count - 1) * (n + 1);
+        for(auto e : v) if(e == count) ans++;
         return max((int)tasks.size(), ans);
     }
 };
