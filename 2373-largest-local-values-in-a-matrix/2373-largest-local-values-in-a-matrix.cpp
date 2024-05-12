b6a28eb26396b7b6dcd78ae4ae23554012812321
Jayant Goel <jgoel92@gmail.com>
2024-05-12 10:09:08 +0530

    Time: 11 ms (53.93%), Space: 13.5 MB (53.31%) - LeetHub

--- 2373-largest-local-values-in-a-matrix/2373-largest-local-values-in-a-matrix.cpp
+++ 2373-largest-local-values-in-a-matrix/2373-largest-local-values-in-a-matrix.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
-    vector<vector<int>> largestLocal(vector<vector<int>>& grid) {
-        vector<vector<int>> v(grid.size()-2, vector<int> (grid[0].size() - 2));
+    int maax(vector<vector<int>> &grid, int x, int y){
+        int maxi = 0;
         
-        for(int i=0;i<grid.size()-2;i++){
-            for(int j=0;j<grid[0].size()-2;j++){
-                
-                int maxi = 0;
-                for(int a = i;a<=i+2;a++){
-                    for(int b=j;b<=j+2;b++){
-                        maxi = max(maxi, grid[a][b]);
-                    }
-                }
-                v[i][j] = maxi;
+        for(int i=x;i<x+3;i++){
+            for(int j=y;j<y+3;j++){
+                maxi = max(maxi, grid[i][j]);
             }
         }
-        return v;
+        
+        return maxi;
+    }
+    vector<vector<int>> largestLocal(vector<vector<int>>& grid) {
+        vector<vector<int>> res(grid.size() - 2, vector<int> (grid.size() - 2));
+        
+        for(int i=0;i<grid.size()-2;i++){
+            for(int j=0;j<grid.size()-2;j++){
+                res[i][j] = maax(grid, i, j);
+            }
+        }
+        
+        return res;
     }
 };
