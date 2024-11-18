9545271ec3e50e707afd1894f8c251c8fbab2c24
Jayant Goel <jgoel92@gmail.com>
2024-11-18 15:16:52 +0530

    Time: 0 ms (100%), Space: 10.4 MB (17.93%) - LeetHub

---
+++ 1755-defuse-the-bomb/1755-defuse-the-bomb.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> decrypt(vector<int>& code, int k) {
+        vector<int> result(code.size(), 0);
+        if (k == 0) return result;
+        // Define the initial window and initial sum
+        int start = 1, end = k, sum = 0;
+        // If k < 0, the starting point will be end of the array.
+        if (k < 0) {
+            start = code.size() - abs(k);
+            end = code.size() - 1;
+        }
+        for (int i = start; i <= end; i++) sum += code[i];
+        // Scan through the code array as i moving to the right, update the
+        // window sum.
+        for (int i = 0; i < code.size(); i++) {
+            result[i] = sum;
+            sum -= code[start % code.size()];
+            sum += code[(end + 1) % code.size()];
+            start++;
+            end++;
+        }
+        return result;
+    }
+};
