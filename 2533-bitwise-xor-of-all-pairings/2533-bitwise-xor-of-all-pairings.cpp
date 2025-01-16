f487992d3d2eb375ebb08bbe6416fac5078cdec5
Jayant Goel <jgoel92@gmail.com>
2025-01-16 11:26:44 +0530

    Time: 3 ms (12.2%), Space: 64.3 MB (64.63%) - LeetHub

---
+++ 2533-bitwise-xor-of-all-pairings/2533-bitwise-xor-of-all-pairings.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
+        // Initialize XOR results for both arrays
+        int xor1 = 0;
+        int xor2 = 0;
+
+        // Get lengths of both arrays
+        int len1 = nums1.size();
+        int len2 = nums2.size();
+
+        // If nums2 length is odd, each element in nums1 appears odd times in
+        // final result
+        if (len2 % 2 != 0) {
+            for (int num : nums1) {
+                xor1 ^= num;
+            }
+        }
+
+        // If nums1 length is odd, each element in nums2 appears odd times in
+        // final result
+        if (len1 % 2 != 0) {
+            for (int num : nums2) {
+                xor2 ^= num;
+            }
+        }
+
+        // Return XOR of both results
+        return xor1 ^ xor2;
+    }
+};
