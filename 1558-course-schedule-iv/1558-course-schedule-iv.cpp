342dcab21dd8bda1ac1e0cb8601d9f8c01f2f1fa
Jayant Goel <jgoel92@gmail.com>
2025-01-27 13:23:42 +0530

    Time: 117 ms (27.42%), Space: 71 MB (56.09%) - LeetHub

---
+++ 1558-course-schedule-iv/1558-course-schedule-iv.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<bool> checkIfPrerequisite(int numCourses,
+                                     vector<vector<int>>& prerequisites,
+                                     vector<vector<int>>& queries) {
+        vector<vector<bool>> isPrerequisite(numCourses,
+                                            vector<bool>(numCourses, false));
+        for (auto edge : prerequisites) {
+            isPrerequisite[edge[0]][edge[1]] = true;
+        }
+
+        for (int intermediate = 0; intermediate < numCourses; intermediate++) {
+            for (int src = 0; src < numCourses; src++) {
+                for (int target = 0; target < numCourses; target++) {
+                    // If src -> intermediate & intermediate -> target exists
+                    // then src -> target will also exist.
+                    isPrerequisite[src][target] =
+                        isPrerequisite[src][target] ||
+                        (isPrerequisite[src][intermediate] &&
+                         isPrerequisite[intermediate][target]);
+                }
+            }
+        }
+
+        vector<bool> answer;
+        for (auto q : queries) {
+            answer.push_back(isPrerequisite[q[0]][q[1]]);
+        }
+
+        return answer;
+    }
+};
