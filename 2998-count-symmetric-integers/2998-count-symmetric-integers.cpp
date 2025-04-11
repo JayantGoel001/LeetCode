f55a2fb664cc169b7f38e904fc6e0b74bc9ae2a5
Jayant Goel <jgoel92@gmail.com>
2025-04-11 12:41:18 +0530

    Time: 4 ms (91.74%), Space: 9.5 MB (90.63%) - LeetHub

---
+++ 2998-count-symmetric-integers/2998-count-symmetric-integers.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int countSymmetricIntegers(int low, int high) {
+        int res = 0;
+        for (int a = low; a <= high; ++a) {
+            if (a < 100 && a % 11 == 0) {
+                res++;
+            } else if (1000 <= a && a < 10000) {
+                int left = a / 1000 + (a % 1000) / 100;
+                int right = (a % 100) / 10 + a % 10;
+                if (left == right) {
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+};
