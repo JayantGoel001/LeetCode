6cedf506f0bc04c9fec12f4f992a478e3278a3bb
Jayant Goel <jgoel92@gmail.com>
2025-04-22 11:24:01 +0530

    Time: 3 ms (91.74%), Space: 9.9 MB (65.14%) - LeetHub

---
+++ 2415-count-the-number-of-ideal-arrays/2415-count-the-number-of-ideal-arrays.cpp
@@ -0,0 +1,51 @@
+const int MOD = 1e9 + 7, MAX_N = 1e4 + 10,
+          MAX_P = 15;  // There are up to 15 prime factors
+int c[MAX_N + MAX_P][MAX_P + 1];
+vector<int> ps[MAX_N];  // List of prime factor counts
+int sieve[MAX_N];       // Minimum prime factor
+
+class Solution {
+public:
+    Solution() {
+        if (c[0][0]) {
+            return;
+        }
+        for (int i = 2; i < MAX_N; i++) {
+            if (sieve[i] == 0) {
+                for (int j = i; j < MAX_N; j += i) {
+                    sieve[j] = i;
+                }
+            }
+        }
+        for (int i = 2; i < MAX_N; i++) {
+            int x = i;
+            while (x > 1) {
+                int p = sieve[x];
+                int cnt = 0;
+                while (x % p == 0) {
+                    x /= p;
+                    cnt++;
+                }
+                ps[i].push_back(cnt);
+            }
+        }
+        c[0][0] = 1;
+        for (int i = 1; i < MAX_N + MAX_P; i++) {
+            c[i][0] = 1;
+            for (int j = 1; j <= min(i, MAX_P); j++) {
+                c[i][j] = (c[i - 1][j] + c[i - 1][j - 1]) % MOD;
+            }
+        }
+    }
+    int idealArrays(int n, int maxValue) {
+        long long ans = 0;
+        for (int x = 1; x <= maxValue; x++) {
+            long long mul = 1;
+            for (int p : ps[x]) {
+                mul = mul * c[n + p - 1][p] % MOD;
+            }
+            ans = (ans + mul) % MOD;
+        }
+        return ans;
+    }
+};
