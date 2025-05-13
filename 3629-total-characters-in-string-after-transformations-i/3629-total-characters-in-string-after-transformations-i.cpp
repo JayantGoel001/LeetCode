55da2246ec627926dc3e2e51710fa93b9d1fd3d9
Jayant Goel <jgoel92@gmail.com>
2025-05-13 15:21:52 +0530

    Time: 583 ms (53.7%), Space: 494.7 MB (18.72%) - LeetHub

---
+++ 3629-total-characters-in-string-after-transformations-i/3629-total-characters-in-string-after-transformations-i.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int lengthAfterTransformations(string s, int t) {
+        vector<int> cnt(26);
+        for (char ch : s) {
+            ++cnt[ch - 'a'];
+        }
+        for (int round = 0; round < t; ++round) {
+            vector<int> nxt(26);
+            nxt[0] = cnt[25];
+            nxt[1] = (cnt[25] + cnt[0]) % mod;
+            for (int i = 2; i < 26; ++i) {
+                nxt[i] = cnt[i - 1];
+            }
+            cnt = move(nxt);
+        }
+        int ans = 0;
+        for (int i = 0; i < 26; ++i) {
+            ans = (ans + cnt[i]) % mod;
+        }
+        return ans;
+    }
+
+private:
+    static constexpr int mod = 1000000007;
+};
