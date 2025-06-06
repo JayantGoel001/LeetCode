8f3ee8def72883018750513ee2df9540a1f2086a
Jayant Goel <jgoel92@gmail.com>
2025-06-06 16:49:14 +0530

    Time: 124 ms (20.3%), Space: 33.7 MB (56.93%) - LeetHub

---
+++ 2520-using-a-robot-to-print-the-lexicographically-smallest-string/2520-using-a-robot-to-print-the-lexicographically-smallest-string.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string robotWithString(string s) {
+        unordered_map<char, int> cnt;
+        for (char c : s) {
+            cnt[c]++;
+        }
+
+        stack<char> stk;
+        string res;
+        char minCharacter = 'a';
+        for (char c : s) {
+            stk.emplace(c);
+            cnt[c]--;
+            while (minCharacter != 'z' && cnt[minCharacter] == 0) {
+                minCharacter++;
+            }
+            while (!stk.empty() && stk.top() <= minCharacter) {
+                res.push_back(stk.top());
+                stk.pop();
+            }
+        }
+
+        return res;
+    }
+};
