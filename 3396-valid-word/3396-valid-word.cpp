086473e32dc15f809b775d778943e17b23864fde
Jayant Goel <jgoel92@gmail.com>
2025-07-15 10:58:00 +0530

    Time: 0 ms (100%), Space: 8.1 MB (33.01%) - LeetHub

---
+++ 3396-valid-word/3396-valid-word.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string word) {
+        if (word.size() < 3) {
+            return false;
+        }
+        bool has_vowel = false;
+        bool has_consonant = false;
+        for (auto c : word) {
+            if (isalpha(c)) {
+                c = tolower(c);
+                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                    has_vowel = true;
+                } else {
+                    has_consonant = true;
+                }
+            } else if (!isdigit(c)) {
+                return false;
+            }
+        }
+        return has_vowel && has_consonant;
+    }
+};
