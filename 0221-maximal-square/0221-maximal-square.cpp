185f922af60162601d9468771e9a7209605d6417
Jayant Goel <jgoel92@gmail.com>
2026-03-02 00:00:59 +0530

    Time: 12 ms (42.5%), Space: 27.7 MB (90.89%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -4,15 +4,19 @@ public:
         int n = matrix.size();
         int m = matrix[0].size();
 
-        vector<vector<int>> dp(2, vector<int> (m, 0));
+        vector<int> dp(m, 0);
         int size = 0;
         for(int i=0;i<n;i++) {
+            int prev = dp[0];
             for(int j=0;j<m;j++) {
-                if (i == 0 || j == 0 || matrix[i][j] == '0') dp[i % 2][j] = matrix[i][j] - '0';
-                else {
-                    dp[i % 2][j] = 1 + min({dp[(i - 1) % 2][j], dp[(i - 1) % 2][j - 1], dp[i % 2][j - 1]});
+                int temp = dp[j];
+                if (i == 0 || j == 0 || matrix[i][j] == '0') {
+                    dp[j] = matrix[i][j] - '0';
+                } else {
+                    dp[j] = 1 + min({dp[j], prev, dp[j - 1]});
                 }
-                size = max(size, dp[i % 2][j]);
+                prev = temp;
+                size = max(size, dp[j]);
             }
         }
 
