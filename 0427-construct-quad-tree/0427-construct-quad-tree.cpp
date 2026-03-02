e2807b1450370ca36a833e56bc94437abe2a45d2
Jayant Goel <jgoel92@gmail.com>
2026-03-02 11:19:50 +0530

    Time: 11 ms (53.7%), Space: 18.7 MB (85.25%) - LeetHub

--- 0427-construct-quad-tree/0427-construct-quad-tree.cpp
+++ 0427-construct-quad-tree/0427-construct-quad-tree.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    int allSame(vector<vector<int>> &grid, int x, int y, int z, int w) {
+    int allSame(vector<vector<int>> &grid, int x, int y, int size) {
         int allOnes = true;
         int allZeros = true;
 
-        for(int i=x;i<=z;i++){
-            for(int j=y;j<=w;j++) {
+        for(int i = x;i < x + size;i++){
+            for(int j = y;j < y + size;j++) {
                 allOnes &= (grid[i][j] == 1);
                 allZeros &= (grid[i][j] == 0);
             }
@@ -13,22 +13,22 @@ public:
 
         return allOnes ? 1 : (allZeros ? 0 : -1);
     }
-    Node *construct(vector<vector<int>> &grid, int x, int y, int z, int w) {
-        int isLeaf = allSame(grid,x,y,z,w);
+    Node *construct(vector<vector<int>> &grid, int x, int y, int size) {
+        int isLeaf = allSame(grid,x,y,size);
         if (isLeaf != -1) return new Node(isLeaf, true);
 
         Node *node = new Node(true, false);
-        int midX = x + (z - x) / 2;
-        int midY = y + (w - y) / 2;
+        int midX = x + size / 2;
+        int midY = y + size / 2;
 
-        node->topLeft = construct(grid, x, y, midX, midY);
-        node->topRight = construct(grid, x, midY + 1, midX, w);
-        node->bottomLeft = construct(grid, midX + 1, y, z, midY);
-        node->bottomRight = construct(grid, midX + 1, midY + 1, z, w);
+        node->topLeft = construct(grid, x, y, size/2);
+        node->topRight = construct(grid, x, midY, size/2);
+        node->bottomLeft = construct(grid, midX, y, size/2);
+        node->bottomRight = construct(grid, midX, midY, size/2);
 
         return node;
     }
     Node* construct(vector<vector<int>>& grid) {
-        return construct(grid, 0, 0, grid.size()-1, grid.size()-1);
+        return construct(grid, 0, 0, grid.size());
     }
 };
