fa349281a1b7c5a2d71536ecd8a66f5bbaef69d6
Jayant Goel <jgoel92@gmail.com>
2026-03-02 11:14:22 +0530

    Time: 12 ms (39.69%), Space: 18.7 MB (85.25%) - LeetHub

--- 0427-construct-quad-tree/0427-construct-quad-tree.cpp
+++ 0427-construct-quad-tree/0427-construct-quad-tree.cpp
@@ -1,72 +1,34 @@
-/*
-// Definition for a QuadTree node.
-class Node {
-public:
-    bool val;
-    bool isLeaf;
-    Node* topLeft;
-    Node* topRight;
-    Node* bottomLeft;
-    Node* bottomRight;
-    
-    Node() {
-        val = false;
-        isLeaf = false;
-        topLeft = NULL;
-        topRight = NULL;
-        bottomLeft = NULL;
-        bottomRight = NULL;
-    }
-    
-    Node(bool _val, bool _isLeaf) {
-        val = _val;
-        isLeaf = _isLeaf;
-        topLeft = NULL;
-        topRight = NULL;
-        bottomLeft = NULL;
-        bottomRight = NULL;
-    }
-    
-    Node(bool _val, bool _isLeaf, Node* _topLeft, Node* _topRight, Node* _bottomLeft, Node* _bottomRight) {
-        val = _val;
-        isLeaf = _isLeaf;
-        topLeft = _topLeft;
-        topRight = _topRight;
-        bottomLeft = _bottomLeft;
-        bottomRight = _bottomRight;
-    }
-};
-*/
-
 class Solution {
 public:
-    bool checkAllSame(vector<vector<int>>& grid,int i,int j,int n){
-        int initial = grid[i][j];
-        for(int x = i;x < i + n;x++){
-            for(int y = j;y < j + n;y++){
-                if(grid[x][y] != initial){
-                    return false;
-                }
+    int allSame(vector<vector<int>> &grid, int x, int y, int z, int w) {
+        int allOnes = true;
+        int allZeros = true;
+
+        for(int i=x;i<=z;i++){
+            for(int j=y;j<=w;j++) {
+                allOnes &= (grid[i][j] == 1);
+                allZeros &= (grid[i][j] == 0);
             }
         }
-        return true;
-    }
-    Node *createQuadTree(vector<vector<int>>& grid,int i,int j,int n){
-        Node *root = nullptr;
-        
-        if(checkAllSame(grid, i, j, n)){
-            root = new Node(grid[i][j], true);
-        }else{
-            root = new Node(grid[i][j], false);
 
-            root->topLeft = createQuadTree(grid, i, j, n/2);
-            root->topRight = createQuadTree(grid, i, j + n/2, n/2);
-            root->bottomLeft = createQuadTree(grid, i + n/2, j, n/2);
-            root->bottomRight = createQuadTree(grid, i + n/2, j + n/2, n/2);
-        }
-        return root;
+        return allOnes ? 1 : (allZeros ? 0 : -1);
+    }
+    Node *construct(vector<vector<int>> &grid, int x, int y, int z, int w) {
+        int isLeaf = allSame(grid,x,y,z,w);
+        if (isLeaf != -1) return new Node(isLeaf, true);
+
+        Node *node = new Node(true, false);
+        int midX = x + (z - x) / 2;
+        int midY = y + (w - y) / 2;
+
+        node->topLeft = construct(grid, x, y, midX, midY);
+        node->topRight = construct(grid, x, midY + 1, midX, w);
+        node->bottomLeft = construct(grid, midX + 1, y, z, midY);
+        node->bottomRight = construct(grid, midX + 1, midY + 1, z, w);
+
+        return node;
     }
     Node* construct(vector<vector<int>>& grid) {
-        return createQuadTree(grid, 0, 0, grid.size());
+        return construct(grid, 0, 0, grid.size()-1, grid.size()-1);
     }
 };
