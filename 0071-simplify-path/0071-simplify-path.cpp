8a7805eb512c0eb39c046d979802b4fc1d6719a4
Jayant Goel <jgoel92@gmail.com>
2026-03-04 21:08:05 +0530

    Time: 6 ms (27.6%), Space: 16 MB (7.36%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -1,35 +1,26 @@
 class Solution {
 public:
     string simplifyPath(string path) {
+        int i = 0;
         stack<string> st;
-        string str = "";
-        for(auto it : path){
-            if(it == '/'){
-                if(str == ".."){
-                    if(!st.empty()) {
-                        st.pop();
-                    }
-                }else if(str != "." && str != ""){
-                    st.push(str);
-                }
-                str = "";
-            }else{
-                str += it;
+        while(i < path.size()) {
+            string temp = "";
+            while(i < path.size() && path[i] != '/') {
+                temp.push_back(path[i++]);
             }
+            
+            if (temp == "..") {
+              if (!st.empty()) st.pop();
+            } else if (temp != "." && temp != "") st.push("/" + temp);
+            
+            i++;
         }
-        if(str == ".."){
-            if(!st.empty()) {
-                st.pop();
-            }
-        }else if(str != "." && str != ""){
-            st.push(str);
-        }
-        
-        str = "";
-        while(!st.empty()){
-            str = "/" + st.top() + str;
+        string res = "";
+        while(!st.empty()) {
+            res = st.top() + res;
             st.pop();
         }
-        return str == "" ? "/" : str;
+
+        return res == "" ? "/" : res;
     }
 };
