cc4a599dd14baa547abfb821190452fb7a0bacb2
Jayant Goel <jgoel92@gmail.com>
2026-03-04 21:11:35 +0530

    Time: 3 ms (66.32%), Space: 10.9 MB (75.92%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -2,21 +2,20 @@ class Solution {
 public:
     string simplifyPath(string path) {
         int i = 0;
-        stack<string> st;
+        vector<string> st;
         while(i < path.size()) {
             string temp = "";
             while(i < path.size() && path[i] != '/') {
                 temp.push_back(path[i++]);
             }
-            if (temp != ".." && temp != "." && temp != "") st.push("/" + temp);
-            if (temp == ".." && !st.empty()) st.pop();
+            if (temp != ".." && temp != "." && temp != "") st.push_back("/" + temp);
+            if (temp == ".." && !st.empty()) st.pop_back();
             
             i++;
         }
         string res = "";
-        while(!st.empty()) {
-            res = st.top() + res;
-            st.pop();
+        for(auto it : st) {
+            res += it;
         }
 
         return res == "" ? "/" : res;
