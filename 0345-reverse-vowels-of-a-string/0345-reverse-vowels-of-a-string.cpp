c7f6bdc64b75430071bc6779adc4d15f2ba27895
Jayant Goel <jgoel92@gmail.com>
2026-03-09 23:19:24 +0530

    Time: 4 ms (20.88%), Space: 10.2 MB (75.41%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
-    bool isVowel(char ch){
-        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' ||ch == 'O' || ch == 'U';  
+    bool isVowel(char ch) {
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U';
     }
     string reverseVowels(string s) {
         int i = 0;
-        int j = s.size()-1;
-        
-        while(i < j){
-            if(isVowel(s[i]) && isVowel(s[j])){
-                swap(s[i++],s[j--]);
-            }else if(!isVowel(s[i])){
-                i++;
-            }else{
-                j--;
-            }
+        int j = s.size() - 1;
+
+        while(i < j) {
+            while(i < j && !isVowel(s[i])) i++;
+            while(i < j && !isVowel(s[j])) j--;
+
+            swap(s[i++], s[j--]);
         }
+
         return s;
     }
 };
