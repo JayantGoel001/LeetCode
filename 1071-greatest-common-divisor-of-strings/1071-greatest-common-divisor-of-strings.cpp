38734a13aca52a0a788a353d93284b3d1280fbd1
Jayant Goel <jgoel92@gmail.com>
2026-03-09 23:06:25 +0530

    Time: 0 ms (100%), Space: 9.7 MB (60.42%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -1,33 +1,14 @@
 class Solution {
 public:
-    bool check(string &s, string &str){
-        for(int i=0;i<str.size();i++){
-            int ch = str[i];
-            int n = i;
-            for(int j=0;j < s.size() / str.size();j++){
-                if(s[n] != ch){
-                    return false;
-                }
-                n += str.size();
-            }
-        }
-        return true;
+    int GCD(int x, int y) {
+        if(y == 0) return x;
+
+        return GCD(y, x % y);
     }
     string gcdOfStrings(string str1, string str2) {
-        string str = "";
-        for(int i=0;i<min(str1.size(), str2.size());i++){
-            if(str1[i] == str2[i]){
-                str += str1[i];
-            }else{
-                break;
-            }
-        }
-        while(str1.size() < str.size() || str2.size() < str.size()){
-            str.pop_back();
-        }
-        while(str.size() > 0 && (str1.size() % str.size() != 0 || str2.size() % str.size() != 0 || !check(str1, str) || !check(str2, str))){
-            str.pop_back();
-        }
-        return str;
+        if (str1 + str2 != str2 + str1) return "";
+        int gcd = GCD(str1.size(), str2.size());
+
+        return str1.substr(0, gcd);
     }
 };
