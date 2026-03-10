d8afd605dc73c2657db6a3ad3ece6c5ad687d112
Jayant Goel <jgoel92@gmail.com>
2026-03-10 09:59:22 +0530

    Time: 0 ms (100%), Space: 15.3 MB (77.56%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
@@ -1,27 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    void preorder(TreeNode *root, vector<int> &v){
-        if(root){
-            if(!root->left && !root->right){
-                v.push_back(root->val);
-            }
-            preorder(root->left, v);
-            preorder(root->right, v);
-        }
+    void dfs(TreeNode *root, vector<int> &leaf) {
+        if (!root) return;
+
+        if (!root->left && !root->right) leaf.push_back(root->val);
+
+        dfs(root->left, leaf);
+        dfs(root->right, leaf);
     }
     bool leafSimilar(TreeNode* root1, TreeNode* root2) {
-        vector<int> v1, v2;
-        preorder(root1, v1);
-        preorder(root2, v2);
-        
-        if(v1.size() != v2.size()){
-            return false;
-        }
-        for(int i=0;i<v1.size();i++){
-            if(v1[i] != v2[i]){
-                return false;
-            }
-        }
-        return true;
+        vector<int> leaf1, leaf2;
+
+        dfs(root1, leaf1);
+        dfs(root2, leaf2);
+
+        return leaf1 == leaf2;
     }
 };
