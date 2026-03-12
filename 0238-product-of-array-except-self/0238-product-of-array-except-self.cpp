2d4a2e4b5472c795ae5ba521d63217fe1828520a
Jayant Goel <jgoel92@gmail.com>
2026-03-12 09:10:31 +0530

    Time: 0 ms (100%), Space: 40.2 MB (79.95%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -5,9 +5,7 @@ public:
         for(int i=1;i<nums.size();i++) {
             res[i] = res[i - 1] * nums[i - 1];
         }
-        // [1, 2, 3, 4]
-        // [1, 1, 2, 6]
-        // 
+        
         int prev = 1;
         for(int i=nums.size()-1;i>=0;i--) {
             res[i] *= prev;
