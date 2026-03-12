712161ee01945a119b6cf187ecc07ff9e93300cd
Jayant Goel <jgoel92@gmail.com>
2026-03-12 07:41:39 +0530

    Time: 0 ms (100%), Space: 10.2 MB (77.31%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -2,13 +2,12 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         vector<int> dp(3, 0);
-        dp[0] = nums[0];
-        dp[1] = max(nums.size() > 1 ? nums[1] : 0, nums[0]);
+        dp[1] = nums[0];
 
-        for(int i=2;i<nums.size();i++) {
-            dp[i % 3] = max(nums[i] + dp[(i - 2 + 3) % 3], dp[(i - 1 + 3) % 3]);
+        for(int i=2;i<=nums.size();i++) {
+            dp[i%3] = max(dp[(i - 1)%3], dp[(i - 2)%3] + nums[i - 1]);
         }
 
-        return dp[(nums.size() - 1 + 3) % 3];
+        return dp[nums.size()%3];
     }
 };
