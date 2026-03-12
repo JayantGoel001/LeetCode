7a243efbdd304a1d00fe08fa88411c4f7507a0f7
Jayant Goel <jgoel92@gmail.com>
2026-03-12 09:35:02 +0530

    Time: 11 ms (25.13%), Space: 13.2 MB (60.16%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -7,7 +7,7 @@ public:
         for(int i=1;i<=s.size();i++) {
             dp[s[i-1] - 'a']++;
             if (i >= k) {
-                maxi = max(maxi, dp[0] + dp['e' - 'a'] + dp['i' - 'a'] + dp['o' - 'a'] + dp['u' - 'a']);
+                maxi = max(maxi, dp[0] + dp[4] + dp[8] + dp['o' - 'a'] + dp['u' - 'a']);
                 dp[s[i - k] - 'a']--;
             }
         }
