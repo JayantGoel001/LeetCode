af2bf33583e9695e8993700d03df64456346fc24
Jayant Goel <jgoel92@gmail.com>
2026-03-14 17:12:54 +0530

    Time: 8 ms (93.56%), Space: 64 MB (94.86%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.cpp
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.cpp
@@ -6,10 +6,16 @@ public:
     }
     
     int ping(int t) {
+        while(!q.empty() && q.front() < t-3000) q.pop();
+
         q.push(t);
-        while(!q.empty() && q.front() < t - 3000){
-            q.pop();
-        }
+
         return q.size();
     }
 };
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter* obj = new RecentCounter();
+ * int param_1 = obj->ping(t);
+ */
