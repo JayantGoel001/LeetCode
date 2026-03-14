c58fa15e1db86efece8952bc1a61f4c7ece73c7e
Jayant Goel <jgoel92@gmail.com>
2026-03-14 16:35:57 +0530

    Time: 12 ms (84.48%), Space: 30.2 MB (9.83%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
     string removeStars(string s) {
-        string str = "";
-        for(auto it : s){
-            if(it == '*'){
-                str.pop_back();
-            }else{
-                str.push_back(it);
+        vector<char> v;
+        for(auto it : s) {
+            if (it == '*') {
+                v.pop_back();
+            } else {
+                v.push_back(it);
             }
         }
+
+        string str = "";
+        for(auto it : v) {
+            str.push_back(it);
+        }
+
         return str;
     }
 };
