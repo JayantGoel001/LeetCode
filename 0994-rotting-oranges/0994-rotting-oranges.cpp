e02fb588c33dc6afb0b778a0713cca09feab8eca
Jayant Goel <jgoel92@gmail.com>
2026-03-17 21:35:58 +0530

    Time: 11 ms (6.5%), Space: 19.1 MB (5.94%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    bool isSafe(vector<vector<int>>& grid, int x, int y, int n, int m) {
+        return x >= 0 && x < n && y >= 0 && y < m && grid[x][y] == 1;
+    }
+    int orangesRotting(vector<vector<int>>& grid) {
+        vector<vector<int>> dir = {
+            {0,1},
+            {1, 0},
+            {-1, 0},
+            {0, -1}
+        };
+        int time = 0;
+        queue<vector<int>> q;
+        int n = grid.size();
+        int m = grid[0].size();
+
+        int rottenOranges = 0;
+        int oranges = 0;
+        
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                oranges += grid[i][j] != 0;
+                if (grid[i][j] != 2) continue;
+
+                q.push({i, j});
+            }
+        }
+
+        while(!q.empty()) {
+            int size = q.size();
+            time++;
+            while(size--) {
+                vector<int> top = q.front();
+                q.pop();
+                
+                rottenOranges++;
+                
+                for(auto it : dir) {
+                    if (isSafe(grid, top[0] + it[0], top[1] + it[1], n, m)) {
+                        q.push({top[0] + it[0], top[1] + it[1]});
+                        grid[top[0] + it[0]][top[1] + it[1]] = 3;
+                    }
+                }
+            }
+        }
+        
+        return rottenOranges == oranges ? max(time - 1, 0) : -1;
+    }
+};
