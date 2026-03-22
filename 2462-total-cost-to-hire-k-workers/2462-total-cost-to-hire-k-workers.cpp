346eaf9a11401082fe1bb7646b26068c9c33c703
Jayant Goel <jgoel92@gmail.com>
2026-03-22 23:46:29 +0530

    Time: 79 ms (8.71%), Space: 79.5 MB (31.65%) - LeetHub

--- 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
@@ -1,24 +1,41 @@
+#define pii pair<int,int>
 class Solution {
 public:
     long long totalCost(vector<int>& costs, int k, int candidates) {
-        priority_queue<int,vector<int>,greater<int>> pq1,pq2;
-        long long ans=0;
-        int cnt = 0,i=0,j=costs.size()-1;
-        while(cnt<k){
-            while(pq1.size()<candidates && i<=j) pq1.push(costs[i++]);
-            while(pq2.size()<candidates && j>=i) pq2.push(costs[j--]);
-            int a=pq1.size()>0?pq1.top():INT_MAX;
-            int b=pq2.size()>0?pq2.top():INT_MAX;
-            // cout<<a<<" "<<b<<"\n";
-            if(a<=b){
-                ans+=a;
-                pq1.pop();
-            }else{
-                ans+=b;
-                pq2.pop();
-            }
-            cnt++;
+        int end = 0;
+        int start = costs.size()-1;
+
+        auto cmp = [&](const pii x, const pii y)->bool{
+            if (costs[x.first] == costs[y.first]) return x.first > y.first;
+            return costs[x.first] > costs[y.first];
+        };
+
+        priority_queue<pii, vector<pii>, decltype(cmp)> pq(cmp);
+        int i = 0;
+        while(i++ < candidates) {
+            pq.push({end++, 1});
         }
+        i = 0;
+        while(i++ < candidates && end <= start) {
+            pq.push({start--, -1});
+        }
+        
+        long long ans = 0ll;
+        while(k--) {
+            pii top = pq.top();
+            pq.pop();
+
+            ans += costs[top.first];
+
+            if (end > start) continue;
+
+            if (top.second == 1) {
+                pq.push({end++, 1});
+            } else {
+                pq.push({start--, -1});
+            }
+        }
+
         return ans;
     }
 };
