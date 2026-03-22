2b428fc2f1fdf1e94814ca080d5c8e37176ceff9
Jayant Goel <jgoel92@gmail.com>
2026-03-22 17:56:14 +0530

    Time: 8 ms (46.9%), Space: 10.4 MB (97.35%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -22,23 +22,26 @@ public:
         int n = word1.size();
         int m = word2.size();
 
-        vector<vector<int>> dp(2, vector<int> (m + 1, 0));
+        vector<int> dp(m + 1, 0);
 
         for(int j=1;j<=m;j++) {
-            dp[0][j] = j;
+            dp[j] = j;
         }
 
         for(int i=1;i<=n;i++) {
-            dp[i%2][0] = i;
+            int prev = dp[0];
+            dp[0] = i;
             for(int j=1;j<=m;j++) {
+                int temp = dp[j];
                 if (word1[i - 1] == word2[j - 1]) {
-                    dp[i % 2][j] = dp[(i - 1) % 2][j - 1];
+                    dp[j] = prev;
                 } else {
-                    dp[i % 2][j] = 1 + min({dp[(i - 1) % 2][j - 1],dp[(i - 1) % 2][j],dp[i % 2][j - 1]});
+                    dp[j] = 1 + min({prev, dp[j], dp[j - 1]});
                 }
+                prev = temp;
             }
         }
 
-        return dp[n % 2][m];
+        return dp[m];
     }
 };
