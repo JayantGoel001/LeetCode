c72736db3cba7483b0ec9f2dffcb176ee96e709c
Jayant Goel <jgoel92@gmail.com>
2026-03-23 21:45:15 +0530

    Time: 3 ms (94.99%), Space: 23.1 MB (12.28%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
-    long long getHours(vector<int> &piles, int k) {
-        long long count = 0;
+    bool getHours(vector<int> &piles, int k, int h) {
+        int count = 0;
         for(auto it : piles) {
-            count += ceil((double)it/k);
+            count += it/k + !!(it % k);
+            if (count > h) return false;
         }
-        return count;
+        return true;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
         int low = 1;
@@ -15,7 +16,7 @@ public:
         while(low <= high) {
             int mid = low + (high - low)/2;
 
-            if (getHours(piles, mid) <= (long long)h) {
+            if (getHours(piles, mid, h)) {
                 ans = mid;
                 high = mid - 1;
             } else {
