593fb345cd8cee41358fdfc6395da7b5ea50d929
Jayant Goel <jgoel92@gmail.com>
2026-03-28 18:48:41 +0530

    Time: 2 ms (23.49%), Space: 9.3 MB (72.45%) - LeetHub

--- 0394-decode-string/0394-decode-string.cpp
+++ 0394-decode-string/0394-decode-string.cpp
@@ -1,31 +1,36 @@
+#define psi pair<string, int>
 class Solution {
 public:
-    string decode(string s,int &pos) {
+    string getString(string &s, int num) {
         string res = "";
-        
-        while(pos < s.size() && s[pos] != ']'){
-            if(s[pos] >= '0' && s[pos] <= '9'){
-                int num = 0;
-
-                while(pos < s.size() && s[pos] >= '0' && s[pos] <= '9'){
-                    num = num * 10 + (s[pos] - '0');
-                    pos++;
-                }
-                pos++;
-                string t = decode(s, pos);
-                pos++;
-                
-                while(num-- > 0){
-                    res += t;
-                }
-            }else{
-                res += s[pos++];
-            }
+        while(num--) {
+            res += s;
         }
         return res;
     }
     string decodeString(string s) {
-        int pos = 0;
-        return decode(s,pos);
+        string curr = "";
+        
+        stack<psi> st;
+        int i = 0;
+        while(i < s.size()) {
+            int num = 0;
+            while (i < s.size() && s[i] >= '0' && s[i] <= '9') {
+                num = num * 10 + (s[i] - '0');
+                i++;
+            }
+            if (s[i] == '[') {
+                st.push({curr, num});
+                curr = "";
+            } else if (s[i] == ']') {
+                psi top = st.top();
+                st.pop();
+                curr = top.first + getString(curr, top.second);
+            } else {
+                curr.push_back(s[i]);
+            }
+            i++;
+        }
+        return curr;
     }
 };
