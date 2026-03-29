74efbad75645109c8e9bcb3333f6f91b29dc618e
Jayant Goel <jgoel92@gmail.com>
2026-03-29 17:03:01 +0530

    Time: 2 ms (23.96%), Space: 12 MB (59.67%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.cpp
+++ 0399-evaluate-division/0399-evaluate-division.cpp
@@ -1,66 +1,39 @@
 class Solution {
 public:
-void dfs(string start,string end,map<string,double>& mp,map<string,vector<string>>& graph,double& val,map<string,int>& visited,bool& found){
-        visited[start]=1;
-    
-        if(found==true)
-            return ;
-        for(auto child:graph[start]){
-            if(visited[child]!=1){
-                // cout<<start<<" "<<child<<"\n";
-                val*=mp[start+"->"+child];
-                if(end==child){
-                    // cout<<end<<" -- "<<child<<"\n";
-                    found=true;
-                    return ;
-                }
-                dfs(child,end,mp,graph,val,visited,found);
-                if(found==true){
-                    return ;
-                }
-                else{
-                    val/=mp[start+"->"+child];
-                }
-            }
-        }
-       
-        
-    }
     vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
-        vector<double> ans;
-        map<string,double> mp;
-        map<string,vector<string>> graph;
-        for(int i=0;i<equations.size();i++){
-            string u=equations[i][0];
-            string v=equations[i][1];
-            mp[u+"->"+v]=values[i];
-            mp[v+"->"+u]=1/values[i];
-            graph[u].push_back(v);
-            graph[v].push_back(u);
+        unordered_map<string,int> mp;
+        int size = 1;
+        for(auto it : equations) {
+            if (!mp[it[0]]) mp[it[0]] = size++;
+            if (!mp[it[1]]) mp[it[1]] = size++;
         }
-        for(int i=0;i<queries.size();i++){
-            string start=queries[i][0];
-            string end=queries[i][1];
-            if(graph.find(start)==graph.end()||graph.find(end)==graph.end()){
-                ans.push_back(-1);
-            }
-            else{
-                // ans.push_back(1);
-                double val=1;
-                map<string,int> visited;
-                bool found=false;
-                if(start==end){
-                    found=true;
+
+        vector<vector<double>> grid(size, vector<double> (size, -1));
+
+        for(int i=0;i<values.size();i++) {
+            grid[mp[equations[i][0]]][mp[equations[i][1]]] = values[i];
+            grid[mp[equations[i][1]]][mp[equations[i][0]]] = 1 / values[i];
+        }
+
+        for(int k=1;k<size;k++) {
+            for(int i=1;i<size;i++) {
+                for(int j=1;j<size;j++) {
+                    if (grid[i][j] != -1 || grid[i][k] == -1 || grid[k][j] == -1) continue;
+
+                    grid[i][j] = grid[i][k] * grid[k][j];
                 }
-                else
-                dfs(start,end,mp,graph,val,visited,found);
-                if(found==true)
-                ans.push_back(val);
-                else
-                    ans.push_back(-1);
-                
             }
         }
-        return ans;
+        vector<double> res;
+        for(auto it : queries) {
+            if (!mp[it[0]] || !mp[it[1]]) {
+                res.push_back(-1.0);
+                continue;
+            }
+
+            res.push_back(grid[mp[it[0]]][mp[it[1]]]);
+        }
+
+        return res;
     }
 };
