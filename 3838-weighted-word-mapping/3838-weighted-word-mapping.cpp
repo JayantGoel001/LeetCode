116fcda5b98771c9be7e2c07160e8f387c48a2ae
Jayant Goel <jgoel92@gmail.com>
2026-06-13 11:05:25 +0530

    Time: 0 ms (100%), Space: 43.6 MB (51.01%) - LeetHub

---
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        string ans;
+        ans.reserve(words.size());
+        for (string word : words) {
+            int s = 0;
+            for (char c : word) {
+                s += weights[c - 'a'];
+            }
+            ans += 'z' - s % 26;
+        }
+        return ans;
+    }
+};
