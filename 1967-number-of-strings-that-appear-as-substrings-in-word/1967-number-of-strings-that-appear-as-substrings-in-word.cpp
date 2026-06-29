bd3acd20492867cf44ffb01a8d3b55594440c076
Jayant Goel <jgoel92@gmail.com>
2026-06-29 13:04:22 +0530

    Time: 7 ms (11.17%), Space: 13.2 MB (9.95%) - LeetHub

--- 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.cpp
+++ 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.cpp
@@ -1,10 +1,39 @@
 class Solution {
 public:
     int numOfStrings(vector<string>& patterns, string word) {
-        int count = 0;
-        for(auto it : patterns){
-            count += word.find(it) != string::npos;
+        auto check = [](const string& pattern, const string& word) -> bool {
+            int m = pattern.size();
+            int n = word.size();
+            // generate the prefix array of the pattern
+            vector<int> pi(m);
+            for (int i = 1, j = 0; i < m; i++) {
+                while (j > 0 && pattern[i] != pattern[j]) {
+                    j = pi[j - 1];
+                }
+                if (pattern[i] == pattern[j]) {
+                    ++j;
+                }
+                pi[i] = j;
+            }
+            // using prefix arrays for matching
+            for (int i = 0, j = 0; i < n; i++) {
+                while (j > 0 && word[i] != pattern[j]) {
+                    j = pi[j - 1];
+                }
+                if (word[i] == pattern[j]) {
+                    ++j;
+                }
+                if (j == m) {
+                    return true;
+                }
+            }
+            return false;
+        };
+
+        int res = 0;
+        for (const string& pattern : patterns) {
+            res += check(pattern, word);
         }
-        return count;
+        return res;
     }
 };
