cdb810f7cd111c1da1ca63abe5648e2adf5e3f32
Jayant Goel <jgoel92@gmail.com>
2026-07-05 13:18:35 +0530

    Time: 8 ms (66.2%), Space: 11.4 MB (78.4%) - LeetHub

---
+++ 1301-number-of-paths-with-max-score/1301-number-of-paths-with-max-score.cpp
@@ -0,0 +1,43 @@
+using PII = pair<int, int>;
+
+class Solution {
+private:
+    static constexpr int mod = (int)1e9 + 7;
+
+public:
+    void update(vector<vector<PII>>& dp, int n, int x, int y, int u, int v) {
+        if (u >= n || v >= n || dp[u][v].first == -1) {
+            return;
+        }
+        if (dp[u][v].first > dp[x][y].first) {
+            dp[x][y] = dp[u][v];
+        } else if (dp[u][v].first == dp[x][y].first) {
+            dp[x][y].second += dp[u][v].second;
+            if (dp[x][y].second >= mod) {
+                dp[x][y].second -= mod;
+            }
+        }
+    }
+
+    vector<int> pathsWithMaxScore(vector<string>& board) {
+        int n = board.size();
+        vector<vector<PII>> dp(n, vector<PII>(n, {-1, 0}));
+        dp[n - 1][n - 1] = {0, 1};
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = n - 1; j >= 0; --j) {
+                if (!(i == n - 1 && j == n - 1) && board[i][j] != 'X') {
+                    update(dp, n, i, j, i + 1, j);
+                    update(dp, n, i, j, i, j + 1);
+                    update(dp, n, i, j, i + 1, j + 1);
+                    if (dp[i][j].first != -1) {
+                        dp[i][j].first +=
+                            (board[i][j] == 'E' ? 0 : board[i][j] - '0');
+                    }
+                }
+            }
+        }
+        return dp[0][0].first == -1
+                   ? vector<int>{0, 0}
+                   : vector<int>{dp[0][0].first, dp[0][0].second};
+    }
+};
