a3bc16afd65119401be3ba6bd7d5b8104cafcb34
Jayant Goel <jgoel92@gmail.com>
2026-08-31 22:54:16 +0530

    Time: 3 ms (67.35%), Space: 124.5 MB (81.63%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -1,31 +1,48 @@
 class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
-        vector<int> critical;
-        
-        int i = 0;
-        ListNode *temp = head;
-        ListNode *prev = nullptr;
-        while(temp){
-            if(prev!=nullptr && temp->next!=nullptr){
-                if((prev->val < temp->val && temp->val > temp->next->val) || (prev->val > temp->val && temp->val < temp->next->val)){
-                    critical.push_back(i);
+        vector<int> result = {-1, -1};
+
+        // Initialize minimum distance to the maximum possible value
+        int minDistance = INT_MAX;
+
+        // Pointers to track the previous node, current node, and indices
+        ListNode* previousNode = head;
+        ListNode* currentNode = head->next;
+        int currentIndex = 1;
+        int previousCriticalIndex = 0;
+        int firstCriticalIndex = 0;
+
+        while (currentNode->next != nullptr) {
+            // Check if the current node is a local maxima or minima
+            if ((currentNode->val < previousNode->val &&
+                 currentNode->val < currentNode->next->val) ||
+                (currentNode->val > previousNode->val &&
+                 currentNode->val > currentNode->next->val)) {
+                // If this is the first critical point found
+                if (previousCriticalIndex == 0) {
+                    previousCriticalIndex = currentIndex;
+                    firstCriticalIndex = currentIndex;
+                } else {
+                    // Calculate the minimum distance between critical points
+                    minDistance =
+                        min(minDistance, currentIndex - previousCriticalIndex);
+                    previousCriticalIndex = currentIndex;
                 }
             }
-            i++;
-            prev = temp;
-            temp = temp->next;
+
+            // Move to the next node and update indices
+            currentIndex++;
+            previousNode = currentNode;
+            currentNode = currentNode->next;
         }
-        vector<int> res(2);
-        if(critical.size() <= 1){
-            return {-1,-1};
+
+        // If at least two critical points were found
+        if (minDistance != INT_MAX) {
+            int maxDistance = previousCriticalIndex - firstCriticalIndex;
+            result = {minDistance, maxDistance};
         }
-        res[0] = INT_MAX;
-        for(int i=0;i<critical.size()-1;i++){
-            res[0] = min(res[0],critical[i+1] - critical[i]);
-        }
-        res[1] = critical.back() - critical[0];
-        
-        return res;
+
+        return result;
     }
 };
