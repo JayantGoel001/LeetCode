cd936867b8e6779c4c8f5efdd10b630abb396da8
Jayant Goel <jgoel92@gmail.com>
2026-09-10 11:17:19 +0530

    Time: 0 ms (100%), Space: 15.7 MB (64.72%) - LeetHub

--- 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
@@ -1,19 +1,19 @@
-#define pii pair<int,int>
+#define pii pair<int, int>
 class Solution {
 public:
     int count = 0;
-    pii inorder(TreeNode *root){
-        if(root){
+    pii inorder(TreeNode* root) {
+        if (root) {
             pii left = inorder(root->left);
             pii right = inorder(root->right);
-            
+
             int sum = left.first + right.first + root->val;
             int c = left.second + right.second + 1;
-            
-            count += (sum/c == root->val);
+
+            count += (sum / c == root->val);
             return {sum, c};
         }
-        return {0,0};
+        return {0, 0};
     }
     int averageOfSubtree(TreeNode* root) {
         inorder(root);
